66f472fdf29d4fe2f802f7f149d2c73c9a429ca8
art1988 <art.khassanov@gmail.com>
2020-10-06 19:33:21 +0500

    were added CKEditorActive and ParagraphActionsPopup

---
+++ ParleyPro Autotests/src/main/java/pages/subelements/CKEditorActive.java
@@ -0,0 +1,85 @@
+package pages.subelements;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
+import utils.Waiter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class CKEditorActive
+{
+    private SelenideElement editorArea = $(".editor-area");
+
+
+    private static Logger logger = Logger.getLogger(CKEditorActive.class);
+
+    public CKEditorActive()
+    {
+        editorArea.waitUntil(Condition.visible, 5_000).shouldBe(Condition.visible);
+
+        Waiter.smartWaitUntilVisible("$('.create-discussion__comment .editor-area')");
+    }
+
+    /**
+     * Sets comment inside active CKEDITOR
+     * @param comment
+     */
+    public void setComment(String comment)
+    {
+        StringBuffer jsCode = new StringBuffer("var names = [];");
+        jsCode.append("for (var i in CKEDITOR.instances) { names.push(CKEDITOR.instances[i]) }");
+        jsCode.append("var editor_instance = names[0];");
+        jsCode.append("var comment_instance = names[1];");
+        jsCode.append("comment_instance.setData('" + comment + "')");
+
+        Selenide.executeJavaScript(jsCode.toString());
+
+        try
+        {
+            Thread.sleep(1_000); // 1 second delay - necessary for correct saving of comment
+        }
+        catch (InterruptedException e)
+        {
+            logger.error("InterruptedException", e);
+        }
+    }
+
+    /**
+     * Click by Internal radio button
+     */
+    public void selectInternal()
+    {
+        Selenide.executeJavaScript("$('.create-discussion__states label:contains(\"Internal\")').parent().find(\"input\").click()");
+
+        logger.info("Internal radio button was selected...");
+    }
+
+    /**
+     * Click by Queued radio button
+     */
+    public void selectQueued()
+    {
+        Selenide.executeJavaScript("$('.create-discussion__states label:contains(\"Que\")').parent().find(\"input\").click()");
+
+        logger.info("Queued radio button was selected...");
+    }
+
+    /**
+     * Click by External radio button
+     */
+    public void selectExternal()
+    {
+        Selenide.executeJavaScript("$('.create-discussion__states label:contains(\"Exter\")').parent().find(\"input\").click()");
+
+        logger.info("External radio button was selected...");
+    }
+
+    public void clickPost()
+    {
+        Selenide.executeJavaScript("$('.create-discussion__foot-content button[class*=create-discussion__submit]').click()");
+
+        logger.info("POST button was clicked...");
+    }
+}
---
+++ ParleyPro Autotests/src/main/java/pages/subelements/ParagraphActionsPopup.java
@@ -0,0 +1,45 @@
+package pages.subelements;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class ParagraphActionsPopup
+{
+    private SelenideElement insertAboveButton    = $(".js-paragraph-tooltip-delete");
+    private SelenideElement insertBelowButton    = $(".js-paragraph-tooltip-insert-below");
+    private SelenideElement deleteButton         = $(".js-paragraph-tooltip-delete");
+    private SelenideElement deleteMultipleButton = $(".js-paragraph-tooltip-delete-multiple");
+    private SelenideElement commentButton        = $(".js-paragraph-tooltip-comment");
+
+
+
+    private static Logger logger = Logger.getLogger(ParagraphActionsPopup.class);
+
+    public ParagraphActionsPopup()
+    {
+        // Assert that there are 5 buttons
+        $$(".rc-tooltip-inner button").shouldHave(CollectionCondition.size(5));
+    }
+
+    public CKEditorActive clickDelete()
+    {
+        deleteButton.click();
+
+        logger.info("Delete button was clicked...");
+
+        return new CKEditorActive();
+    }
+
+    public CKEditorActive clickAddComment()
+    {
+        commentButton.click();
+
+        logger.info("Add comment button was clicked...");
+
+        return new CKEditorActive();
+    }
+}
