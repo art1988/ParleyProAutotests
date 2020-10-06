b7d109bea459e7af86c0e29397ceee8a9d712988
art1988 <art.khassanov@gmail.com>
2020-10-06 09:59:58 +0500

    switch to OS independent file separator

--- ParleyPro Autotests/src/main/java/constants/Const.java
+++ ParleyPro Autotests/src/main/java/constants/Const.java
@@ -8,8 +8,8 @@ public class Const
 {
     public static final String QA_TENANT_URL           = "http://qa-autotests.parleypro.net/master/index.html";
     public static final User   PREDEFINED_USER_CN_ROLE = new User("autotest_cn fn", "ln", "arthur.khasanov+autotestcn@parleypro.com", "Parley650!");
-    public static final File   CONTRACT_SAMPLE         = new File(System.getProperty("user.dir") + "\\Contracts\\pramata.docx");
-    public static final File   DOWNLOAD_DIR            = new File(System.getProperty("user.dir") + "\\Downloads");
+    public static final File   CONTRACT_SAMPLE         = new File(System.getProperty("user.dir") + "/Contracts/pramata.docx");
+    public static final File   DOWNLOAD_DIR            = new File(System.getProperty("user.dir") + "/Downloads");
 
 
     private Const() {}
