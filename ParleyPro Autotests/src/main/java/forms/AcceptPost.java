7c99db4aa6d4ac18e87c145403ff1753e90aea84
art1988 <art.khassanov@gmail.com>
2020-10-08 17:11:20 +0500

    AcceptPost: changes in constructor and clicking accept action due to PAR-12360

--- ParleyPro Autotests/src/main/java/forms/AcceptPost.java
+++ ParleyPro Autotests/src/main/java/forms/AcceptPost.java
@@ -1,9 +1,12 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import constants.AcceptTypes;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
+import utils.Waiter;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,8 +16,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class AcceptPost
 {
     private AcceptTypes     acceptType;
-    private SelenideElement title            = $(".modal-body-title");
-    private SelenideElement acceptTextButton = $(".discussion2-close-confirm__body .btn-primary");
 
 
     private static Logger logger = Logger.getLogger(AcceptPost.class);
@@ -23,13 +24,18 @@ public class AcceptPost
     {
         this.acceptType = acceptType;
 
-        title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText(acceptType.getTitle()));
-        $(".modal-body-description").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText(acceptType.getMessage()));
+        // TODO: need to change locators after fixing of https://parley.atlassian.net/browse/PAR-12360
+        Waiter.smartWaitUntilVisible("$('.modal-title, .modal-body-title')");
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.modal-title, .modal-body-title').text()"), acceptType.getTitle());
+
+        Waiter.smartWaitUntilVisible("$('.modal-body-description, .modal-description')");
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.modal-body-description, .modal-description').text()"), acceptType.getMessage());
     }
 
     public void clickAcceptText()
     {
-        acceptTextButton.click();
+        // TODO: need to change locators after fixing of https://parley.atlassian.net/browse/PAR-12360
+        Selenide.executeJavaScript("$('.discussion2-close-confirm__body .btn-primary, ._button__text:contains(\"Accept text\")').click()");
 
         logger.info("Accept text button was clicked...");
     }
