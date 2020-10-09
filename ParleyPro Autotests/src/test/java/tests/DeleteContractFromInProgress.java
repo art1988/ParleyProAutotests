78cb49aa89f5abcb8dddc6ffd2426d5467fde997
art1988 <art.khassanov@gmail.com>
2020-10-09 17:58:14 +0500

    was added test DeleteContractFromInProgress

---
+++ ParleyPro Autotests/src/test/java/tests/DeleteContractFromInProgress.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.InProgressContractsPage;
+import pages.OpenedContract;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DeleteContractFromInProgress
+{
+    private static Logger logger = Logger.getLogger(DeleteContractFromExecuted.class);
+
+    @Test
+    @Parameters("contractName")
+    public void deleteContractFromInProgress(String contractName)
+    {
+        DashboardPage dashboardPage = new DashboardPage();
+
+        InProgressContractsPage inProgressContractsPage = dashboardPage.getSideBar().clickInProgressContracts(false);
+
+        inProgressContractsPage.selectContract(contractName);
+
+        OpenedContract openedContract = new OpenedContract();
+
+        openedContract.clickActionsMenu().clickDeleteContract().clickDelete();
+
+        logger.info("Assert delete notification...");
+        $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Contract " + contractName + " has been deleted."));
+
+        logger.info("Assert that In-progress contracts page has blank page with no contracts...");
+        inProgressContractsPage = new InProgressContractsPage(true);
+
+        Screenshoter.makeScreenshot();
+    }
+}
