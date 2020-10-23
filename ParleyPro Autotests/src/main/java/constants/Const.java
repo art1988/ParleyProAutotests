483c99d52b6aff4d7dc07890ddb319183cf45819
art1988 <art.khassanov@gmail.com>
2020-10-23 14:24:20 +0500

    Const: beautify

--- ParleyPro Autotests/src/main/java/constants/Const.java
+++ ParleyPro Autotests/src/main/java/constants/Const.java
@@ -6,18 +6,17 @@ import java.io.File;
 
 public class Const
 {
-    public static final String QA_TENANT_URL                = "http://qa-autotests.parleypro.net/master/index.html";
-    public static final User   PREDEFINED_USER_CN_ROLE      = new User("autotest_cn fn", "ln", "arthur.khasanov+autotestcn@parleypro.com", "Parley650!");
-
+    public static final String QA_TENANT_URL                 = "http://qa-autotests.parleypro.net/master/index.html";
+    public static final User   PREDEFINED_USER_CN_ROLE       = new User("autotest_cn fn", "ln", "arthur.khasanov+autotestcn@parleypro.com", "Parley650!");
     public static final File   CONTRACT_LIFECYCLE_SAMPLE     = new File(System.getProperty("user.dir") + "/Contracts/pramata.docx");
     public static final File   CONTRACT_DISCUSSIONS_SAMPLE   = new File(System.getProperty("user.dir") + "/Contracts/AT-14.docx");
     public static final File   CONTRACT_FORMATTING_SAMPLE    = new File(System.getProperty("user.dir") + "/Contracts/Formatting.docx");
     public static final File   CONTRACT_NUMBERED_LIST_SAMPLE = new File(System.getProperty("user.dir") + "/Contracts/NumberedList.docx");
 
-    public static final File   DOWNLOAD_DIR                 = new File(System.getProperty("user.dir") + "/Downloads");
+    public static final File   DOWNLOAD_DIR                  = new File(System.getProperty("user.dir") + "/Downloads");
 
     // Dir for screenshots of failed tests
-    public static final File   SCREENSHOTS_DIR              = new File(System.getProperty("user.dir") + "/ScreenshotsOfFailedTests");
+    public static final File   SCREENSHOTS_DIR               = new File(System.getProperty("user.dir") + "/ScreenshotsOfFailedTests");
 
 
     private Const() {}
