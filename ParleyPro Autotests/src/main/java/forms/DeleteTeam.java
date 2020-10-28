a279883add09a9fd868c71b9e9c564df451b50d4
art1988 <art.khassanov@gmail.com>
2020-10-28 16:58:24 +0500

    were added page objects DeleteTeam and TeamActionMenu

---
+++ ParleyPro Autotests/src/main/java/forms/DeleteTeam.java
@@ -0,0 +1,29 @@
+package forms;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DeleteTeam
+{
+    private SelenideElement title = $(".modal-body-title");
+    private SelenideElement deleteButton = $(".button.btn-common.btn.btn-danger");
+
+
+    private static Logger logger = Logger.getLogger(DeleteTeam.class);
+
+    public DeleteTeam()
+    {
+        title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Delete team"));
+        $(".modal-body-description").shouldHave(Condition.exactText("Are you sure to delete team"));
+    }
+
+    public void clickDelete()
+    {
+        deleteButton.click();
+
+        logger.info("Delete button was clicked...");
+    }
+}
---
+++ ParleyPro Autotests/src/main/java/pages/tooltips/TeamActionMenu.java
@@ -0,0 +1,43 @@
+package pages.tooltips;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import forms.AddNewTeam;
+import forms.DeleteTeam;
+import org.apache.log4j.Logger;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+/**
+ * Class that represents tooltip with Edit and Delete buttons
+ */
+public class TeamActionMenu
+{
+    private static Logger logger = Logger.getLogger(TeamActionMenu.class);
+
+    public TeamActionMenu()
+    {
+        $(".dropdown.open.btn-group ul").waitUntil(Condition.visible, 6_000);
+        $$(".dropdown.open.btn-group ul li").shouldHaveSize(3).shouldHave(CollectionCondition.exactTexts("Edit", "", "Delete"));
+    }
+
+    public AddNewTeam clickEdit()
+    {
+        Selenide.executeJavaScript("$('.dropdown.open.btn-group ul').find('li:contains(\"Edit\")').find('a')[0].click()");
+
+        logger.info("Edit team menu was clicked...");
+
+        return new AddNewTeam();
+    }
+
+    public DeleteTeam clickDelete()
+    {
+        Selenide.executeJavaScript("$('.dropdown.open.btn-group ul').find('li:contains(\"Delete\")').find('a')[0].click()");
+
+        logger.info("Delete team menu was clicked...");
+
+        return new DeleteTeam();
+    }
+}
