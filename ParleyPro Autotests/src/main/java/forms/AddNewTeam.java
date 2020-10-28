3efbb2ec711533268b58df5dabd2172dc75de8b9
art1988 <art.khassanov@gmail.com>
2020-10-28 16:59:11 +0500

    AddNewTeam: were added methods deleteUser and clickSave

--- ParleyPro Autotests/src/main/java/forms/AddNewTeam.java
+++ ParleyPro Autotests/src/main/java/forms/AddNewTeam.java
@@ -1,25 +1,29 @@
 package forms;
 
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
+import pages.administration.Teams;
 
 import static com.codeborne.selenide.Selenide.$;
 
+/**
+ * Represents page that visible after clicking of + NEW TEAM button or by clicking Edit of existing team
+ */
 public class AddNewTeam
 {
     private SelenideElement teamNameFiled        = $("input[label='Team name']");
     private SelenideElement teamDescriptionField = $(".textarea.input__input");
     private SelenideElement addMembersButton     = $("._button.scheme_gray.size_md");
+    private SelenideElement saveButton           = $("#editTeamSave");
+    private SelenideElement cancelButton         = $("#editTeamCancel");
 
 
     private static Logger logger = Logger.getLogger(AddNewTeam.class);
 
     public AddNewTeam()
     {
-        $(".team-info__name").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Team name"));
-
         // check that it has gear image
         boolean hasImage = $(".team-icon").getCssValue("background-image").contains("images/d8f66bdcf20bfedb3a09672478d1516b.svg");
 
@@ -43,5 +47,28 @@ public class AddNewTeam
         logger.info("+ Add Members button was clicked");
 
         return new AddMembers();
+    }
+
+    /**
+     * Delete user from team
+     * @param userName name of user to be deleted
+     */
+    public void deleteUser(String userName)
+    {
+        // click actions menu
+        Selenide.executeJavaScript("$('.teams-users-list .teams-users-list__name:contains(\"" + userName + "\")').next().next().next().find(\"#actions-menu\").click()");
+        // click Delete
+        Selenide.executeJavaScript("$('.teams-users-list .teams-users-list__name:contains(\"" + userName + "\")').next().next().next().find(\"#actions-menu\").next().find(\"a\")[0].click()");
+
+        logger.info("User " + userName + " was deleted...");
+    }
+
+    public Teams clickSave()
+    {
+        saveButton.click();
+
+        logger.info("Save button was clicked");
+
+        return new Teams();
     }
 }
