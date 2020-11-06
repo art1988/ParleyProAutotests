efb6a73704f7deb72a3789e1833deb28ecadb977
art1988 <art.khassanov@gmail.com>
2020-11-06 15:53:13 +0500

    Workflows : was added editApprovalWorkflow method

--- ParleyPro Autotests/src/main/java/pages/administration/Workflows.java
+++ ParleyPro Autotests/src/main/java/pages/administration/Workflows.java
@@ -3,8 +3,10 @@ package pages.administration;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import forms.ApprovalWorkflow;
 import org.apache.log4j.Logger;
-import pages.tooltips.WorkflowActionsMenu;
+import pages.tooltips.EditWorkflowActionsMenu;
+import pages.tooltips.NewWorkflowActionsMenu;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -23,12 +25,24 @@ public class Workflows
         $(".workflows-list__row.type_header").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Name\nType of flow\nContract Category\nContract Type\nDepartment\nDate"));
     }
 
-    public WorkflowActionsMenu clickAddNewWorkflow()
+    public NewWorkflowActionsMenu clickAddNewWorkflow()
     {
         Selenide.executeJavaScript("$('.btn.btn-light.button_type_add').click()");
 
         logger.info("+ New Workflow button was clicked");
 
-        return new WorkflowActionsMenu();
+        return new NewWorkflowActionsMenu();
+    }
+
+    /**
+     * Click 3 dots button for the given workflow name
+     * @param workflowName
+     * @return
+     */
+    public EditWorkflowActionsMenu editApprovalWorkflow(String workflowName)
+    {
+        Selenide.executeJavaScript("$('.workflows-list .workflows-list__row .type_name:contains(\"" + workflowName + "\")').parent().find(\".type_actions button\").click()");
+
+        return new EditWorkflowActionsMenu();
     }
 }
