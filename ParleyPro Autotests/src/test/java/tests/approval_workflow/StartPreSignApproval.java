8fbbd5c5d87fb8ffee389e428830d9988f78a33c
art1988 <art.khassanov@gmail.com>
2020-11-12 19:51:19 +0500

    StartPreSignApproval : were added tests loginAsTeamMemeber2, loginAsApproverUser2AndApprove, loginAsTeamMemeber2AndApprove, loginAsCNAndVerifyCheckmarks, checkAuditTrail

--- ParleyPro Autotests/src/test/java/tests/approval_workflow/StartPreSignApproval.java
+++ ParleyPro Autotests/src/test/java/tests/approval_workflow/StartPreSignApproval.java
@@ -2,14 +2,19 @@ package tests.approval_workflow;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import constants.Const;
 import forms.ConfirmApprovers;
 import forms.EmailWillBeSentToTheCounterparty;
+import forms.SignContract;
 import forms.StartNegotiation;
 import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.OpenedContract;
+import pages.*;
 import utils.Screenshoter;
+import utils.Waiter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -55,9 +60,151 @@ public class StartPreSignApproval
 
         confirmApproversForm.clickStartApproval();
 
+        Waiter.smartWaitUntilVisible("$('.document__header-row span[title]:contains(\"pramata\")').parent().parent().parent().next().find('.lifecycle:contains(\"APPROVAL\")')");
+
         logger.info("Assert that approvers icons are visible: Approval_User_2 and Team#2");
         $(".header-users .user").waitUntil(Condition.appear, 15_000); // wait until users icons will appear
         $$(".header-users .user").shouldHave(CollectionCondition.size(1)).shouldHave(CollectionCondition.exactTexts("A")); // one User...
         $$(".header-users .team").shouldHave(CollectionCondition.size(1)); // ...and one Team
+
+        Screenshoter.makeScreenshot();
+
+        LoginPage loginPage = new DashboardPage().getSideBar().logout(); // Logout
+    }
+
+    @Test(priority = 3)
+    @Description("This test logins as Team 2 member and verifies that he can’t approve")
+    public void loginAsTeamMemeber2()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_INTERNAL_USER_2.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_INTERNAL_USER_2.getPassword() );
+
+        logger.info("Making sure that Dashboard was loaded correctly...");
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+        InProgressContractsPage inProgressContractsPage = dashboardPage.getSideBar().clickInProgressContracts(false);
+
+        inProgressContractsPage.selectContract("Approval workflow positive");
+        OpenedContract openedContract = new OpenedContract();
+
+        logger.info("Assert that Approve and Reject buttons are not present...");
+
+        boolean approveButtonDoesntExist = Selenide.executeJavaScript("return ($('#APPROVE_DOCUMENT').length === 0)"),
+                rejectButtonDoesntExist  = Selenide.executeJavaScript("return ($('#REJECT_DOCUMENT').length === 0)");
+        Assert.assertTrue(approveButtonDoesntExist && rejectButtonDoesntExist);
+
+        Screenshoter.makeScreenshot();
+
+        loginPage = new DashboardPage().getSideBar().logout(); // Logout
+    }
+
+    @Test(priority = 4)
+    @Description("This test logins as Approval_User_2 and approves document")
+    public void loginAsApproverUser2AndApprove()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_APPROVER_USER_2.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_APPROVER_USER_2.getPassword() );
+
+        logger.info("Making sure that Dashboard was loaded correctly...");
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+        InProgressContractsPage inProgressContractsPage = dashboardPage.getSideBar().clickInProgressContracts(false);
+
+        inProgressContractsPage.selectContract("Approval workflow positive");
+        OpenedContract openedContract = new OpenedContract();
+
+        logger.info("Assert that Approve and Reject buttons are present...");
+        boolean approveButtonDoesntExist = Selenide.executeJavaScript("return ($('#APPROVE_DOCUMENT').length === 0)"),
+                rejectButtonDoesntExist  = Selenide.executeJavaScript("return ($('#REJECT_DOCUMENT').length === 0)");
+        Assert.assertFalse(approveButtonDoesntExist && rejectButtonDoesntExist);
+
+        openedContract.clickApproveButton("pramata").clickApproveButton(); // Click approve
+
+        Screenshoter.makeScreenshot();
+
+        loginPage = new DashboardPage().getSideBar().logout(); // Logout
+    }
+
+    @Test(priority = 5)
+    @Description("This test logins as Team 2 member and approves document")
+    public void loginAsTeamMemeber2AndApprove()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_INTERNAL_USER_2.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_INTERNAL_USER_2.getPassword() );
+
+        logger.info("Making sure that Dashboard was loaded correctly...");
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+        InProgressContractsPage inProgressContractsPage = dashboardPage.getSideBar().clickInProgressContracts(false);
+
+        inProgressContractsPage.selectContract("Approval workflow positive");
+        OpenedContract openedContract = new OpenedContract();
+
+        logger.info("Assert that Approve and Reject buttons are present...");
+        boolean approveButtonDoesntExist = Selenide.executeJavaScript("return ($('#APPROVE_DOCUMENT').length === 0)"),
+                rejectButtonDoesntExist  = Selenide.executeJavaScript("return ($('#REJECT_DOCUMENT').length === 0)");
+        Assert.assertFalse(approveButtonDoesntExist && rejectButtonDoesntExist);
+
+        openedContract.clickApproveButton("pramata").clickApproveButton(); // Click approve
+
+        Screenshoter.makeScreenshot();
+
+        loginPage = new DashboardPage().getSideBar().logout(); // Logout
+    }
+
+    @Test(priority = 6)
+    @Description("This test logins as Chief Negotiator ( under main user ) and verify approvers checkmarks")
+    public void loginAsCNAndVerifyCheckmarks()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_USER_CN_ROLE.getPassword() );
+
+        logger.info("Making sure that Dashboard was loaded correctly...");
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+        InProgressContractsPage inProgressContractsPage = dashboardPage.getSideBar().clickInProgressContracts(false);
+
+        inProgressContractsPage.selectContract("Approval workflow positive");
+        OpenedContract openedContract = new OpenedContract();
+
+        logger.info("Assert that approvers icons have checkmarks...");
+        $(".header-users .user").waitUntil(Condition.appear, 10_000); // wait until users icons will appear
+        Assert.assertTrue(Selenide.executeJavaScript("return $('.team-icon.team').hasClass(\"_show-checkbox_yes\")")); // team has checkmark
+        $$(".header-users .user .user__checkbox").shouldHave(CollectionCondition.size(1)); // user has checkmark
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 7)
+    @Description("This test opens Audit trail and checks that all messages are correct")
+    public void checkAuditTrail()
+    {
+        OpenedContract openedContract = new OpenedContract();
+
+        AuditTrail auditTrailPage = openedContract.clickAuditTrail();
+
+        logger.info("Assert that all messages are correct");
+        String actual = Selenide.executeJavaScript("return $('.timeline-heading h4').text()");
+        Assert.assertEquals(actual, "Approval completedApproved by teamTeam request sentApprovedUser approve request was " +
+                "sentPre-signature approval startedDocument user assignedDocument user assignedDocument user assignedOnline negotiation " +
+                "startedApproval completedApprovedUser approve request was sentApprovedUser approve request was sentPre-negotiation approval " +
+                "startedDocument user assignedDocument user assignedDocument uploadedContract created");
+
+        Screenshoter.makeScreenshot();
+
+        auditTrailPage.clickOk();
+    }
+
+    @Test(priority = 8)
+    @Description("This test move document to Sign stage and ")
+    public void moveAndCompleteSign()
+    {
+        OpenedContract openedContract = new OpenedContract();
+
+        SignContract signContractForm = openedContract.switchDocumentToSign("Approval workflow positive");
     }
 }
