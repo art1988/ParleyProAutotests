4fe9164bf1fbe1261048a4f0189c42f6fb21fd61
art1988 <art.khassanov@gmail.com>
2020-11-23 15:50:50 +0500

    finished with test CreateContractPositiveForContractRoutingWorkflow

--- ParleyPro Autotests/src/test/java/tests/routing_workflow/CreateContractPositiveForContractRoutingWorkflow.java
+++ ParleyPro Autotests/src/test/java/tests/routing_workflow/CreateContractPositiveForContractRoutingWorkflow.java
@@ -3,25 +3,35 @@ package tests.routing_workflow;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import constants.Const;
+import forms.ContractInNegotiation;
 import forms.ContractInformation;
 import forms.StartReview;
 import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.AddDocuments;
+import pages.AuditTrail;
 import pages.InProgressContractsPage;
 import pages.OpenedContract;
 import pages.subelements.CKEditorActive;
 import utils.Screenshoter;
 import utils.Waiter;
 
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class CreateContractPositiveForContractRoutingWorkflow
 {
+    private static String contractName = "Contract routing workflow positive";
     private static Logger logger = Logger.getLogger(CreateContractPositiveForContractRoutingWorkflow.class);
+
 
     @Test(priority = 1)
     @Description("This test creates contract that satisfies Contract Routing Workflow settings")
@@ -32,7 +42,7 @@ public class CreateContractPositiveForContractRoutingWorkflow
 
         ContractInformation contractInformationForm = inProgressContractsPage.clickNewContractButton();
 
-        contractInformationForm.setContractTitle("Contract routing workflow positive");
+        contractInformationForm.setContractTitle(contractName);
         contractInformationForm.setContractCurrency("EUR");
         contractInformationForm.setContractValue("22450"); // in appropriate range between [15400, 32700]
         contractInformationForm.setContractingRegion("region1");
@@ -94,15 +104,17 @@ public class CreateContractPositiveForContractRoutingWorkflow
         Selenide.executeJavaScript(jsCode.toString());
 
         $(".contract-user__status").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Reviewer"));
+        $(".contract-user__name").waitUntil(Condition.visible, 6_000)
+                .shouldHave(Condition.exactText(Const.PREDEFINED_INTERNAL_USER_1.getFirstName() + " " + Const.PREDEFINED_INTERNAL_USER_1.getLastName()));
+
+        Screenshoter.makeScreenshot();
 
         // Hide active hover menu
         Selenide.executeJavaScript("$('.contract-user__section').hide()");
-
-        Screenshoter.makeScreenshot();
     }
 
     @Test(priority = 4)
-    @Description("")
+    @Description("This test verifies that Internal user2 was added to document after deletion of paragraph and has reviewer role")
     public void addDiscussionAndCheckThatUser2WasAdded() throws InterruptedException
     {
         OpenedContract openedContract = new OpenedContract();
@@ -115,5 +127,87 @@ public class CreateContractPositiveForContractRoutingWorkflow
         logger.info("Assert that internal discussion notification was shown...");
         $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Internal discussion " + paragraphTitle + " has been successfully created."));
         $(".notification-stack").waitUntil(Condition.disappear, 15_000);
+
+        logger.info("Assert that user icons are visible...");
+        $(".header-users .user").waitUntil(Condition.appear, 25_000); // wait until users icons will appear
+        $$(".header-users .user").shouldHave(CollectionCondition.size(3)).shouldHave(CollectionCondition.exactTexts("A", "I", "I"));
+
+        // Hover over second I user icon
+        StringBuffer jsCode = new StringBuffer("var event = new MouseEvent('mouseover', {bubbles: true, cancelable: true});");
+        jsCode.append("$('.header-users span:contains(\"I\")')[1].dispatchEvent(event);");
+        Selenide.executeJavaScript(jsCode.toString());
+
+        $(".contract-user__status").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Reviewer"));
+        $(".contract-user__name").waitUntil(Condition.visible, 6_000)
+                .shouldHave(Condition.exactText(Const.PREDEFINED_INTERNAL_USER_2.getFirstName() + " " + Const.PREDEFINED_INTERNAL_USER_2.getLastName()));
+
+        Screenshoter.makeScreenshot();
+
+        // Hide active hover menu
+        Selenide.executeJavaScript("$('.contract-user__section').hide()");
+    }
+
+    @Test(priority = 5)
+    @Description("This test uploads Counterparty document and verifies that Approval_User_2 was added to document and has Lead role")
+    public void uploadCounterpartyDocument()
+    {
+        OpenedContract openedContract = new OpenedContract();
+
+        AddDocuments addDocumentsPage = openedContract.clickNewDocument();
+
+        // Upload Counterparty documents
+        addDocumentsPage.clickUploadCounterpartyDocuments( Const.CONTRACT_FORMATTING_SAMPLE );
+
+        logger.info("Assert upload notification...");
+        $(".notification-stack").waitUntil(Condition.visible, 25_000).shouldHave(Condition.exactText("Document Formatting has been successfully uploaded."));
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        logger.info("Assert that Contract 'Contract routing workflow positive' is now in Negotiation popup is visible...");
+        new ContractInNegotiation(contractName).clickOk();
+
+        logger.info("Assert that just uploaded document has 2 added user icons...");
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.document__header-rename:contains(\"Formatting\")').parent().parent().parent().find(\".header-users .user\").text()"),
+                "aA");
+
+        // Hover over second I user icon
+        StringBuffer jsCode = new StringBuffer("var event = new MouseEvent('mouseover', {bubbles: true, cancelable: true});");
+        jsCode.append("$('.document__header-rename:contains(\"Formatting\")').parent().parent().parent().find(\".header-users .user\")[1].dispatchEvent(event);");
+        Selenide.executeJavaScript(jsCode.toString());
+
+        $(".contract-user__status").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Lead"));
+        $(".contract-user__name").waitUntil(Condition.visible, 6_000)
+                .shouldHave(Condition.exactText(Const.PREDEFINED_APPROVER_USER_2.getFirstName()));
+
+        logger.info("Assert that document has 3RD PARTY mark...");
+        $(".label.label_theme_dgray").shouldHave(Condition.exactText("3RD PARTY"));
+
+        logger.info("Assert that button SEND INVITE is visible...");
+        $("._button.scheme_blue.size_tier2").shouldHave(Condition.exactText("SEND INVITE"));
+
+        Screenshoter.makeScreenshot();
+
+        // Hide active hover menu
+        Selenide.executeJavaScript("$('.contract-user__section').hide()");
+
+        logger.info("Assert that contract header has 4 added users...");
+        $$(".contract-header__status .user").shouldHave(CollectionCondition.size(4));
+    }
+
+    @Test(priority = 6)
+    @Description("This test opens Audit trail and checks Document user assigned events")
+    public void checkAuditTrailEvents()
+    {
+        OpenedContract openedContract = new OpenedContract();
+
+        AuditTrail auditTrail = openedContract.clickAuditTrail();
+
+        logger.info("Assert that there are 3 'Document user assigned' events...");
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.timeline-title:contains(\"Document user assigned\")').length"), Long.valueOf(3));
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.timeline-title:contains(\"Document user assigned\")').parent().parent().find('.timeline-body:contains(\"User\")').text()"),
+                "User: arthur.khasanov+approval2@parleypro.comUser: arthur.khasanov+team2@parleypro.comUser: arthur.khasanov+team1@parleypro.com");
+
+        Screenshoter.makeScreenshot();
+
+        auditTrail.clickOk();
     }
 }
