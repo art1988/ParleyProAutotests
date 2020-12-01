954b2933832c88be4de35f9a1ee69cc40a037bbe
art1988 <art.khassanov@gmail.com>
2020-12-01 19:11:51 +0500

    Discussions was renamed to DiscussionsOfSingleContract;

--- ParleyPro Autotests/src/main/java/pages/DiscussionsOfSingleContract.java
+++ ParleyPro Autotests/src/main/java/pages/DiscussionsOfSingleContract.java
@@ -2,18 +2,22 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
 
 /**
- * Represents opened DISCUSSIONS tab within contract
+ * Represents opened DISCUSSIONS tab within one selected contract
  */
-public class Discussions
+public class DiscussionsOfSingleContract
 {
     private SelenideElement contractTitle = $(".contract-header__name");
+    private SelenideElement documentsTab  = $(".menu-disc-doc__documents");
 
 
-    public Discussions(String contractName)
+    private static Logger logger = Logger.getLogger(DiscussionsOfSingleContract.class);
+
+    public DiscussionsOfSingleContract(String contractName)
     {
         $(".spinner").waitUntil(Condition.disappear, 15_000);
         contractTitle.waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText(contractName));
@@ -22,5 +26,14 @@ public class Discussions
     public String getDiscussionCount()
     {
         return $(".discussions-info .discussion-indicator__count").getText();
+    }
+
+    public OpenedContract clickDocumentsTab()
+    {
+        documentsTab.click();
+
+        logger.info("DOCUMENTS tab was clicked");
+
+        return new OpenedContract();
     }
 }
