684b4a98a84425c4b643c0019cc5cd0f61da1d92
art1988 <art.khassanov@gmail.com>
2020-12-04 19:47:31 +0500

    was added test CreateContractForOnlineFormattingAT20

---
+++ ParleyPro Autotests/src/test/java/tests/formatting/CreateContractForOnlineFormattingAT20.java
@@ -0,0 +1,51 @@
+package tests.formatting;
+
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import forms.ContractInformation;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.AddDocuments;
+import pages.InProgressContractsPage;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+import utils.Waiter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ ScreenShotOnFailListener.class})
+public class CreateContractForOnlineFormattingAT20
+{
+    @Test
+    public void createContractForOnlineFormatting()
+    {
+        // 1. + NEW CONTRACT
+        InProgressContractsPage inProgressContractsPage = new InProgressContractsPage(true);
+
+        ContractInformation contractInformationForm = inProgressContractsPage.clickNewContractButton();
+
+        contractInformationForm.setContractTitle("AT20 online formatting contract");
+        contractInformationForm.setContractingRegion("region1");
+        contractInformationForm.setContractingCountry("country1");
+        contractInformationForm.setContractEntity("entity1");
+        contractInformationForm.setContractingDepartment("department1");
+        contractInformationForm.setContractCategory("category1");
+        contractInformationForm.setContractType("type1");
+
+        contractInformationForm.clickSave();
+
+        // 2. UPLOAD MY TEAM DOCUMENTS
+        AddDocuments addDocuments = new AddDocuments();
+
+        addDocuments.clickUploadMyTeamDocuments( Const.CONTRACT_CLASSIC_AT40 );
+
+        // Wait until document is fully loaded...
+        Waiter.smartWaitUntilVisible("$('.document-paragraph__content-text:contains(\"delete me\")')");
+        Waiter.smartWaitUntilVisible("$('.document-paragraph__content-text:contains(\"Unused extra\")')");
+
+        $(".notification-stack").waitUntil(Condition.appear, 6_000).shouldHave(Condition.exactText("Document AT-40 has been successfully uploaded."));
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        Screenshoter.makeScreenshot();
+    }
+}
