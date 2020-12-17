cabb11db52a079f7f9bc541be80d05f55dfa6a8e
art1988 <art.khassanov@gmail.com>
2020-12-17 17:21:05 +0500

    ContractInformation: were added getters and missing setters for fields

--- ParleyPro Autotests/src/main/java/forms/ContractInformation.java
+++ ParleyPro Autotests/src/main/java/forms/ContractInformation.java
@@ -1,6 +1,7 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
@@ -8,10 +9,11 @@ import org.openqa.selenium.Keys;
 import org.testng.Assert;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 /**
  * Represents form that appears after clicking on + NEW CONTRACT button
- * or by clicking on Contract info icon
+ * or by clicking on Contract info icon.
  */
 public class ContractInformation
 {
@@ -22,6 +24,11 @@ public class ContractInformation
     private SelenideElement contractEntityField        = $("#contractEntity");
     private SelenideElement contractingDepartmentField = $("#ContractingDepartment");
     private SelenideElement contractCategoryField      = $("#contractCategory");
+    private SelenideElement tagsField                  = $("#tags");
+    private SelenideElement notesField                 = $("#notes");
+
+    private SelenideElement cancelButton = $(".btn.btn-common.btn-link.btn-link-pseudo");
+    private SelenideElement saveButton   = $(".button.btn.btn-common.btn-blue.btn.btn-default");
 
 
     private static Logger logger = Logger.getLogger(ContractInformation.class);
@@ -66,6 +73,11 @@ public class ContractInformation
         Selenide.executeJavaScript("$('.react-datepicker__day--today').click()");
     }
 
+    public String getDueDate()
+    {
+        return $("#dueDate").getValue();
+    }
+
     /**
      * Set contract currency
      * @param currency may be USD, EUR, GBP, etc.
@@ -83,10 +95,59 @@ public class ContractInformation
         contractValueField.setValue(value);
     }
 
+    public String getContractValue()
+    {
+        return contractValueField.getValue();
+    }
+
+    /**
+     * Set Contract radio button
+     * @param contract may be "Buy", "Sell" or "Other"
+     */
+    public void setContractRadioButton(String contract)
+    {
+        Selenide.executeJavaScript("$('.radio-group__label:contains(\"Contract\")').next().find(\"span:contains('" + contract + "')\").click()");
+    }
+
+    public String getContractRadioButtonSelection()
+    {
+        return Selenide.executeJavaScript("return $('.radio-group__label:contains(\"Contract\")').next().find(\"input[checked]\").next().text()");
+    }
+
+    /**
+     * Set My company template radio button
+     * @param flag true = Yes, false = No
+     */
+    public void setMyCompanyTemplate(boolean flag)
+    {
+        if( flag == true )
+        {
+            Selenide.executeJavaScript("$('.radio-group__label:contains(\"My company template\")').next().find(\"span:contains('Yes')\").click()");
+        }
+        else
+        {
+            Selenide.executeJavaScript("$('.radio-group__label:contains(\"My company template\")').next().find(\"span:contains('No')\").click()");
+        }
+    }
+
+    /**
+     * Get selected value of My company template
+     * @return may be "Yes" or "No"
+     */
+    public String getMyCompanyTemplateRadioButtonSelection()
+    {
+        return Selenide.executeJavaScript("return $('.radio-group__label:contains(\"My company template\")').next().find(\"input[checked]\").next().text()");
+    }
+
     public void setContractingRegion(String region)
     {
         contractingRegionField.sendKeys(Keys.BACK_SPACE); // clear field by pressing BACK_SPACE
         contractingRegionField.setValue(region);
+    }
+
+    public String getContractingRegion()
+    {
+        return contractingRegionField.getValue();
     }
 
     public void setContractingCountry(String country)
@@ -94,9 +155,19 @@ public class ContractInformation
         contractingCountryField.setValue(country);
     }
 
+    public String getContractingCountry()
+    {
+        return contractingCountryField.getValue();
+    }
+
     public void setContractEntity(String entity)
     {
         contractEntityField.setValue(entity);
+    }
+
+    public String getContractEntity()
+    {
+        return contractEntityField.getValue();
     }
 
     public void setContractingDepartment(String department)
@@ -104,10 +175,20 @@ public class ContractInformation
         contractingDepartmentField.setValue(department);
     }
 
+    public String getContractingDepartment()
+    {
+        return contractingDepartmentField.getValue();
+    }
+
     public void setContractCategory(String category)
     {
         contractCategoryField.sendKeys(Keys.BACK_SPACE); // clear field by pressing BACK_SPACE
         contractCategoryField.setValue(category);
+    }
+
+    public String getContractCategory()
+    {
+        return contractCategoryField.getValue();
     }
 
     public void setContractType(String type)
@@ -119,6 +200,11 @@ public class ContractInformation
         Selenide.executeJavaScript("$('span:contains(\"Contract type\")').parent().parent().next().find(\"label:contains('All Types')\").click()");
 
         Selenide.executeJavaScript("$('span:contains(\"Contract type\")').parent().parent().next().find(\"label:contains('" + type + "')\").click()");
+    }
+
+    public String getContractType()
+    {
+        return Selenide.executeJavaScript("return $('input[inputid=\"contractType\"]').val()");
     }
 
     public void checkClassicNegotiationModeCheckbox()
@@ -135,13 +221,52 @@ public class ContractInformation
         return chiefNegotiator;
     }
 
+    public void setTag(String tag) throws InterruptedException
+    {
+        tagsField.setValue(tag);
+        Thread.sleep(500); // necessary sleep to preserve order during adding of multiple tags
+
+        tagsField.pressEnter();
+        Thread.sleep(500); // the same
+    }
+
+    /**
+     * Get tags.
+     * @return ElementsCollection object.
+     */
+    public ElementsCollection getTags()
+    {
+        return $$(".tags-input__tag");
+    }
+
+    public void setNotes(String notes)
+    {
+        notesField.setValue(notes);
+    }
+
+    public String getNotes()
+    {
+        return notesField.getText();
+    }
+
+    public void clickCancel()
+    {
+        cancelButton.waitUntil(Condition.enabled, 5_000).click();
+
+        logger.info("CANCEL button was clicked");
+
+        // wait until Contract information form disappear
+        $(".modal-content").waitUntil(Condition.disappear, 7_000);
+    }
+
     public void clickSave()
     {
         // wait until SAVE button is enabled
-        $(".button.btn.btn-common.btn-blue.btn.btn-default").waitUntil(Condition.enabled, 5_000);
+        saveButton.waitUntil(Condition.enabled, 5_000).click();
 
-        Selenide.executeJavaScript("$('.pull-right button:contains(\"SAVE\")').click()");
+        logger.info("SAVE button was clicked");
 
-        logger.info("Save button was clicked");
+        // wait until Contract information form disappear
+        $(".modal-content").waitUntil(Condition.disappear, 7_000);
     }
 }
