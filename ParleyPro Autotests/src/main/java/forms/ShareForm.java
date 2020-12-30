a0817c16bd278e3d3d9930ef5d37ac1310f41851
art1988 <art.khassanov@gmail.com>
2020-12-30 19:23:09 +0500

    ShareForm: were added additional methods to interact with form

--- ParleyPro Autotests/src/main/java/forms/ShareForm.java
+++ ParleyPro Autotests/src/main/java/forms/ShareForm.java
@@ -1,9 +1,11 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.Keys;
+import pages.tooltips.RoleSelectorTooltip;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -13,6 +15,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class ShareForm
 {
     private SelenideElement addParticipantField = $("#manage-users__select");
+    private SelenideElement addNoteCheckbox     = $(".active .checkbox__image");
+    private SelenideElement messageTextArea     = $(".active textarea");
 
     private SelenideElement sendButton = $("._button.scheme_blue.size_lg.manage-users-new__submit");
     private SelenideElement doneButton = $(".manage-users-added__foot button");
@@ -30,23 +34,50 @@ public class ShareForm
         $(".manage-users-user").waitUntil(Condition.visible, 7_000);
     }
 
-    public ShareForm addParticipant(String nameOrEmail)
+    public ShareForm addParticipant(String nameOrEmail) throws InterruptedException
     {
         addParticipantField.setValue(nameOrEmail);
 
-        try
-        {
-            Thread.sleep(1_000);
-        }
-        catch (InterruptedException e)
-        {
-            logger.error("InterruptedException", e);
-        }
+        Thread.sleep(1_000); // some sleep to make sure that values were populated in dropdown
 
         addParticipantField.sendKeys(Keys.ARROW_DOWN);
         addParticipantField.pressEnter();
 
+        Thread.sleep(1_000); // some sleep during slide animation
+
         return this; // we are on the same form
+    }
+
+    public RoleSelectorTooltip changeRoleOfInternalUser(String username)
+    {
+        Selenide.executeJavaScript("$('.active .manage-users-user__fullname:contains(\"" + username + "\")').parent().parent().next().find(\"button\").click()");
+
+        return new RoleSelectorTooltip(username);
+    }
+
+    /**
+     * Click Resend invite button for given user
+     * @param username
+     */
+    public ResendInviteForm resendInvite(String username)
+    {
+        Selenide.executeJavaScript("$('.manage-users-user__fullname:contains(\"" + username + "\")').parent().parent().find(\"svg\").parent().click()");
+
+        logger.info("Resend invite button was clicked for user: " + username);
+
+        return new ResendInviteForm(this);
+    }
+
+    public void clickAddNoteCheckbox()
+    {
+        addNoteCheckbox.click();
+
+        logger.info("Add a note checkbox was clicked...");
+    }
+
+    public void setMessage(String message)
+    {
+        messageTextArea.sendKeys(message);
     }
 
     public void clickSend()
