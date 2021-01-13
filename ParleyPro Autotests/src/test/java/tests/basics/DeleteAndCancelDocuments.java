617baa623b406c42cca0b3167d1e0c93b3c432ef
art1988 <art.khassanov@gmail.com>
2021-01-13 19:19:09 +0500

    was added test DeleteAndCancelDocuments

---
+++ ParleyPro Autotests/src/test/java/tests/basics/DeleteAndCancelDocuments.java
@@ -0,0 +1,48 @@
+package tests.basics;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+import pages.OpenedContract;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DeleteAndCancelDocuments
+{
+    private static Logger logger = Logger.getLogger(DeleteAndCancelDocuments.class);
+
+    @Test
+    @Description("This test deletes document by the name 'Formatting' and cancel document by the name 'AT-40'")
+    public void deleteAndCancelDocuments()
+    {
+        OpenedContract openedContract = new OpenedContract();
+
+        logger.info("Trying to delete document [Formatting]...");
+        openedContract.clickDocumentActionsMenu("Formatting").clickDelete().clickDelete();
+
+        $(".notification-stack").waitUntil(Condition.appear, 6_000).shouldHave(Condition.exactText("Document Formatting has been deleted."));
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        logger.info("Making sure that only one document left...");
+        $$(".rename.document__header-rename").shouldHave(CollectionCondition.size(1)).shouldHave(CollectionCondition.exactTexts("AT-40"));
+
+        Screenshoter.makeScreenshot();
+
+        logger.info("Trying to cancel document [AT-40]...");
+        openedContract.clickDocumentActionsMenu("AT-40").clickCancel().clickCancel();
+
+        $(".notification-stack").waitUntil(Condition.appear, 6_000).shouldHave(Condition.exactText("Document AT-40 has been cancelled."));
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        logger.info("Making sure that status of document was changed to CANCELLED...");
+        $(".document .lifecycle").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("CANCELLED"));
+
+        // TODO: add check that there is no Cancel option after cancelling after fixing of PAR-13248
+
+        Screenshoter.makeScreenshot();
+    }
+}
