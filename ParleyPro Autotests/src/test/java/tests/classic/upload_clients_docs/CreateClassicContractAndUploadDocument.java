6325af6a8271afcabe4af0997bb48727a8759ea7
art1988 <art.khassanov@gmail.com>
2021-01-14 20:02:47 +0500

    in-progress: were added tests CreateClassicContractAndUploadDocument and UploadNewVersionFromCP

---
+++ ParleyPro Autotests/src/test/java/tests/classic/upload_clients_docs/CreateClassicContractAndUploadDocument.java
@@ -0,0 +1,100 @@
+package tests.classic.upload_clients_docs;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import forms.ContractInformation;
+import forms.EmailWillBeSentToTheCounterparty;
+import forms.StartNegotiation;
+import forms.UploadDocumentDetectedChanges;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.AddDocuments;
+import pages.InProgressContractsPage;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class CreateClassicContractAndUploadDocument
+{
+    private String documentName;
+
+    private static Logger logger = Logger.getLogger(CreateClassicContractAndUploadDocument.class);
+
+    @Test(priority = 1)
+    @Parameters({"documentName", "documentHasRevisions"})
+    public void createClassicContractAndUploadDocument(String documentName, boolean documentHasRevisions) throws InterruptedException
+    {
+        this.documentName = documentName.substring(0, documentName.indexOf("."));
+
+        // 1. + NEW CONTRACT
+        InProgressContractsPage inProgressContractsPage = new InProgressContractsPage(true);
+
+        ContractInformation contractInformationForm = inProgressContractsPage.clickNewContractButton();
+
+        contractInformationForm.setContractTitle("Classic contract - client docs");
+        contractInformationForm.checkClassicNegotiationModeCheckbox();
+        contractInformationForm.setContractingRegion("region1");
+        contractInformationForm.setContractingCountry("country1");
+        contractInformationForm.setContractEntity("entity1");
+        contractInformationForm.setContractingDepartment("department1");
+        contractInformationForm.setContractCategory("category1");
+        contractInformationForm.setContractType("type1");
+
+        contractInformationForm.clickSave();
+
+        String docNameWithoutExtension = documentName.substring(0, documentName.indexOf("."));
+        // 2. UPLOAD MY TEAM DOCUMENTS
+        AddDocuments addDocuments = new AddDocuments();
+        if( documentHasRevisions == true ) // if document has revisions then after uploading it should be in Negotiate status
+        {
+            UploadDocumentDetectedChanges detectedChangesForm = addDocuments.clickUploadMyTeamDocumentsWithDetectedChanges( new File(Const.CLIENT_DOCS_DIR.getAbsolutePath() + "/" + documentName) );
+
+            detectedChangesForm.setCounterpartyOrganization("CounterpartyAT");
+            detectedChangesForm.setCounterpartyNegotiatorEmail("cpName cpLastName (arthur.khasanov+cpat@parleypro.com)");
+            detectedChangesForm.clickOk();
+
+            $(".spinner").waitUntil(Condition.disappear, 25_000);
+            $(".notification-stack").waitUntil(Condition.appear, 6_000).shouldHave(Condition.exactText("Document " + docNameWithoutExtension + " has been successfully uploaded."));
+            $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+        }
+        else // else move it to Negotiate status
+        {
+            addDocuments.clickUploadMyTeamDocuments( new File(Const.CLIENT_DOCS_DIR.getAbsolutePath() + "/" + documentName) );
+
+            $(".spinner").waitUntil(Condition.disappear, 25_000);
+            $(".notification-stack").waitUntil(Condition.appear, 6_000).shouldHave(Condition.exactText("Document " + docNameWithoutExtension + " has been successfully uploaded."));
+            $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+            StartNegotiation startNegotiationForm = new OpenedContract().switchDocumentToNegotiate(docNameWithoutExtension, true);
+            EmailWillBeSentToTheCounterparty emailWillBeSentToTheCounterpartyForm = startNegotiationForm.clickNext(true);
+
+            emailWillBeSentToTheCounterpartyForm.setCounterpartyOrganization("CounterpartyAT");
+            Thread.sleep(1_000);
+            emailWillBeSentToTheCounterpartyForm.setCounterpartyChiefNegotiator("arthur.khasanov+cpat@parleypro.com");
+
+            emailWillBeSentToTheCounterpartyForm.clickStart();
+
+            logger.info("Assert visible to the counterparty notification...");
+            $(".notification-stack").waitUntil(Condition.visible, 10_000).shouldHave(Condition.exactText("Contract Classic contract - client docs is now in negotiation. No notification was sent to the Counterparty."));
+
+            logger.info("Assert that status was changed to NEGOTIATE...");
+            $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("NEGOTIATE\n(1)", "NEGOTIATE"));
+
+            logger.info("Assert that WITH MY TEAM label is visible...");
+            $(".label_theme_orange").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("WITH MY TEAM"));
+
+            Screenshoter.makeScreenshot();
+        }
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/classic/upload_clients_docs/UploadNewVersionFromCP.java
@@ -0,0 +1,36 @@
+package tests.classic.upload_clients_docs;
+
+import constants.Const;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.DiscussionsOfSingleContract;
+import pages.DocumentComparePreview;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+
+import java.io.File;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class UploadNewVersionFromCP
+{
+    private static Logger logger = Logger.getLogger(UploadNewVersionFromCP.class);
+
+    @Test
+    @Parameters({"documentName", "cpDocumentName"})
+    public void uploadNewVersionFromCP(String documentName, String cpDocumentName)
+    {
+        String docNameWithoutExtension = documentName.substring(0, documentName.indexOf("."));
+
+        OpenedContract openedContract = new OpenedContract();
+
+        DocumentComparePreview comparePreview = openedContract.
+                clickUploadNewVersionButton(documentName.substring(0, documentName.indexOf("."))).
+                clickUploadCounterpartyDocument(new File(Const.CLIENT_DOCS_DIR.getAbsolutePath() + "/" + cpDocumentName),
+                                                docNameWithoutExtension,
+                                    "Classic contract - client docs");
+
+        DiscussionsOfSingleContract discussionsOfSingleContract = comparePreview.clickUpload(true);
+    }
+}
