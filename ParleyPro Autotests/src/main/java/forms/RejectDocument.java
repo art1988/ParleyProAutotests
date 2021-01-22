ec59b5e7db820881be7d63989c32827ca4a3d033
art1988 <art.khassanov@gmail.com>
2021-01-22 19:15:33 +0500

    was added RejectDocument page object

---
+++ ParleyPro Autotests/src/main/java/forms/RejectDocument.java
@@ -0,0 +1,36 @@
+package forms;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class RejectDocument
+{
+    private SelenideElement title            = $(".modal-body-title");
+    private SelenideElement commentsTextArea = $(".input textarea");
+    private SelenideElement rejectButton     = $(".button.btn-common.btn.btn-primary");
+
+    private static Logger logger = Logger.getLogger(RejectDocument.class);
+
+
+    public RejectDocument(String documentName)
+    {
+        title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Are you sure you want to reject document \"" + documentName + "\"?"));
+        rejectButton.waitUntil(Condition.visible, 7_000);
+    }
+
+    public void setComments(String comment)
+    {
+        commentsTextArea.sendKeys(comment);
+    }
+
+    public void clickReject()
+    {
+        rejectButton.click();
+
+        logger.info("REJECT button was clicked...");
+    }
+
+}
