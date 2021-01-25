5f094300563f38138e8f112217004a63cf25480d
art1988 <art.khassanov@gmail.com>
2021-01-25 15:08:26 +0500

    finished with test: LoginAsApproverAndReject; was added RemoveWorkflowAndTeams clenup test

--- ParleyPro Autotests/src/test/java/tests/approval_workflow/reject/LoginAsApproverAndReject.java
+++ ParleyPro Autotests/src/test/java/tests/approval_workflow/reject/LoginAsApproverAndReject.java
@@ -1,25 +1,30 @@
 package tests.approval_workflow.reject;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import constants.Const;
 import forms.RejectDocument;
+import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-import pages.DashboardPage;
-import pages.InProgressContractsPage;
-import pages.LoginPage;
-import pages.OpenedContract;
+import pages.*;
+import utils.EmailChecker;
+import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
+@Listeners({ScreenShotOnFailListener.class})
 public class LoginAsApproverAndReject
 {
     private static Logger logger = Logger.getLogger(LoginAsApproverAndReject.class);
 
     @Test(priority = 1)
+    @Description("This test logins as PREDEFINED_APPROVER_USER_1 and make reject")
     public void loginAsApproverAndMakeReject() throws InterruptedException
     {
         LoginPage loginPage = new LoginPage();
@@ -47,5 +52,50 @@ public class LoginAsApproverAndReject
         logger.info("Assert that REJECT button is disabled without comment...");
         Assert.assertTrue($(".button.btn-common.btn.btn-primary").is(Condition.disabled));
         Assert.assertTrue(Selenide.executeJavaScript("return $('.button.btn-common.btn.btn-primary')[0].hasAttribute(\"disabled\")"));
+
+        String rejectComment = "This is simple comment to make reject button available.";
+        rejectDocument.setComments(rejectComment);
+        rejectDocument.clickReject();
+
+        Screenshoter.makeScreenshot();
+
+        logger.info("Assert reject notification...");
+        $(".notification-stack").waitUntil(Condition.visible, 25_000).shouldHave(Condition.exactText("Document pramata has been rejected\nReason: " + rejectComment));
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        logger.info("Check that contract was removed from in-progress list...");
+        $(".contracts__empty-greetings").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Welcome to your contracts!"));
+        $(".contracts__empty-message").waitUntil(Condition.visible, 7_000)
+                .shouldHave(Condition.exactText("You haven't been assigned a role in any contracts.\nYou'll receive a notification when you're assigned a role."));
+
+        dashboardPage.getSideBar().logout();
+    }
+
+    @Test(priority = 2)
+    @Description("This test logins back as CN and check that document status was switched back to review")
+    public void loginBackAsCN()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_USER_CN_ROLE.getPassword() );
+        loginPage.clickSignIn();
+
+        InProgressContractsPage inProgressContractsPage = new DashboardPage().getSideBar().clickInProgressContracts(false);
+        inProgressContractsPage.selectContract("Approval rejection contract");
+
+        logger.info("Assert that document status was switched back to review...");
+        $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("REVIEW\n(1)", "REVIEW"));
+
+        logger.info("Checking audit trail events...");
+        AuditTrail auditTrail = new OpenedContract().clickAuditTrail();
+
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.timeline-title').text()"),
+                "Pre-negotiation approval rejectedUser approve request was sentPre-negotiation approval startedDocument user assignedDocument uploadedContract created");
+        auditTrail.clickOk();
+
+        logger.info("Checking CN e-mail...");
+        EmailChecker.assertEmailBySubject("imap.gmail.com", "arthur.khasanov@parleypro.com", "ParGd881",
+                "Document \"pramata\" rejected by arthur.khasanov+approval1@parleypro.com");
     }
 }
---
+++ ParleyPro Autotests/src/test/java/tests/approval_workflow/reject/RemoveWorkflowAndTeams.java
@@ -0,0 +1,64 @@
+package tests.approval_workflow.reject;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.AdministrationPage;
+import pages.DashboardPage;
+import pages.administration.Teams;
+import pages.administration.Workflows;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class RemoveWorkflowAndTeams
+{
+    private static Logger logger = Logger.getLogger(RemoveWorkflowAndTeams.class);
+
+    @Test(priority = 1)
+    @Description("This test delete workflow: Approval_WFL_AT")
+    public void deleteWorkflow()
+    {
+        Workflows workflowsTabPage = new DashboardPage().getSideBar().clickAdministration().clickWorkflowsTab();
+        workflowsTabPage.clickActionMenu("Approval_WFL_AT").clickDelete().clickDelete();
+
+        try
+        {
+            Thread.sleep(1_000);
+        }
+        catch (InterruptedException e)
+        {
+            logger.error("InterruptedException", e);
+        }
+
+        logger.info("Assert that there is no approval workflow anymore...");
+        Assert.assertTrue(Selenide.executeJavaScript("return $('.workflows-list .workflows-list__row').length === 1"));
+    }
+
+    @Test(priority = 2)
+    @Description("This test delete 2 teams: Autotest_TEAM_3 [EDITED] and Team #2")
+    public void deleteTeams()
+    {
+        Teams teamsTabPage = new AdministrationPage().clickTeamsTab();
+
+        // wait until spinner disappear and list of team should appear
+        $(".spinner").waitUntil(Condition.disappear, 15_000);
+
+        teamsTabPage.clickActionMenu("Autotest_TEAM_3 [EDITED]").clickDelete().clickDelete();
+
+        logger.info("Assert delete team notification...");
+        $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Team deleted successfully."));
+        $(".notification-stack").waitUntil(Condition.disappear, 15_000);
+
+        teamsTabPage.clickActionMenu("Team #2").clickDelete().clickDelete();
+
+        logger.info("Assert delete team notification...");
+        $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Team deleted successfully."));
+        $(".notification-stack").waitUntil(Condition.disappear, 15_000);
+    }
+}
