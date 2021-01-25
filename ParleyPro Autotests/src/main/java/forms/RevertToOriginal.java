4a08cb6b686b0a0d47a86f07cee10a6daa83f6d5
art1988 <art.khassanov@gmail.com>
2021-01-25 15:05:32 +0500

    RevertToOriginal: was rewrited the way of checking of description text

--- ParleyPro Autotests/src/main/java/forms/RevertToOriginal.java
+++ ParleyPro Autotests/src/main/java/forms/RevertToOriginal.java
@@ -1,10 +1,8 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
-import org.testng.Assert;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -19,14 +17,14 @@ public class RevertToOriginal
     public RevertToOriginal()
     {
         title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Are you sure you want to close this discussion?"));
-
-        Assert.assertEquals(Selenide.executeJavaScript("return $('.modal-description').text()"),
-                "The contents of the document won't change because no text changes have been requested as a result of this discussion");
+        $(".modal-description").waitUntil(Condition.visible, 7_000)
+                .shouldHave(Condition.exactText("The contents of the document won't change because no text changes have been requested as a result of this discussion"));
+        closeDiscussionButton.waitUntil(Condition.visible, 7_000);
     }
 
     public void clickCloseDiscussion()
     {
-        closeDiscussionButton.waitUntil(Condition.visible, 6_000).click();
+        closeDiscussionButton.click();
 
         logger.info("Close discussion button was clicked...");
     }
