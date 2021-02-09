a9dc78723fa9123898e48bc010aea5f077cedc18
art1988 <art.khassanov@gmail.com>
2021-02-09 19:10:21 +0500

    was added general parameterized test Delete Template

---
+++ ParleyPro Autotests/src/test/java/tests/DeleteTemplate.java
@@ -0,0 +1,31 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * General test for deleting templates from 'Templates' page
+ */
+@Listeners({ScreenShotOnFailListener.class})
+public class DeleteTemplate
+{
+    private static Logger logger = Logger.getLogger(DeleteTemplate.class);
+
+    @Test
+    @Parameters("templateName")
+    public void deleteTemplate(String templateName)
+    {
+        new DashboardPage().getSideBar().clickTemplates(false).clickActionMenu(templateName).clickDelete().clickDelete();
+
+        logger.info("Assert that delete template notification was shown...");
+        $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Template " + templateName + " has been deleted."));
+        $(".notification-stack").waitUntil(Condition.disappear, 15_000);
+    }
+}
