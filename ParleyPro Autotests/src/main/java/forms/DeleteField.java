c193bea35dfc4d1d939e05dab21b5df68fcb65b5
art1988 <art.khassanov@gmail.com>
2021-02-09 18:58:50 +0500

    Fields: was finished with createNewFiledForSummary method; was added removeField method; was added DeleteField page object

---
+++ ParleyPro Autotests/src/main/java/forms/DeleteField.java
@@ -0,0 +1,35 @@
+package forms;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.apache.log4j.Logger;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Popup that appears after clicking on garbage can for field on Fields page
+ */
+public class DeleteField
+{
+    private SelenideElement deleteButton = $(".button.btn-common.btn.btn-danger");
+
+    private static Logger logger = Logger.getLogger(DeleteField.class);
+
+    public DeleteField()
+    {
+        $(".modal-body-title").waitUntil(Condition.visible, 7_000)
+                .shouldHave(Condition.exactText("Are you sure you want to delete this field?"));
+
+        $(".modal-body-description").waitUntil(Condition.visible, 7_000)
+                .shouldHave(Condition.exactText("The field will be removed from all contracts"));
+    }
+
+    public void clickDelete()
+    {
+        deleteButton.click();
+
+        logger.info("DELETE button was clicked");
+
+        $(".modal-content").waitUntil(Condition.disappear, 7_000);
+    }
+}
--- ParleyPro Autotests/src/main/java/pages/administration/Fields.java
+++ ParleyPro Autotests/src/main/java/pages/administration/Fields.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import constants.FieldType;
+import forms.DeleteField;
 import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -23,6 +24,14 @@ public class Fields
     public Fields()
     {
         $(".spinner").waitUntil(Condition.disappear, 7_000);
+        try
+        {
+            Thread.sleep(1_000);
+        }
+        catch (InterruptedException e)
+        {
+            logger.error("InterruptedException", e);
+        }
 
         $$(".admin-fields-navigation a").shouldHave(CollectionCondition.size(3))
                 .shouldHave(CollectionCondition.exactTexts("CONTRACT FIELDS", "FIELD RELATIONS", "LAYOUT"));
@@ -31,12 +40,33 @@ public class Fields
                 .shouldHave(CollectionCondition.exactTexts("Summary", "Post-execution", "Contract Request"));
     }
 
-    public void createNewFiledForSummary(String fieldName, FieldType fieldType, boolean isRequired) throws InterruptedException
+    public void createNewFiledForSummary(String fieldName, FieldType fieldType, boolean isRequired)
     {
         Selenide.executeJavaScript("$('.admin-fields__title:contains(\"Summary\")').parent().find('.admin-fields-add__button').click()");
 
+        // Set Field name
         $("input[label='Field name'][value='']").waitUntil(Condition.visible, 5_000).sendKeys(fieldName);
-        //$(".Select-multi-value-wrapper").
+
+        // Set Field type
+        Selenide.executeJavaScript("$('.Select-control input').attr('id', 'fieldTypeID')");
+        $("#fieldTypeID").setValue(fieldType.getFieldType()).pressEnter();
+
+        // Set Required field
+        if( isRequired == true )
+        {
+            $("input[label='Field name'][value='" + fieldName + "']").parent().parent().parent().parent().find(".checkbox.with_label").click();
+        }
+    }
+
+    /**
+     * Removes field by it's name by clicking garbage icon.
+     * @param fieldName
+     */
+    public DeleteField removeField(String fieldName)
+    {
+        Selenide.executeJavaScript("$('input[label=\"Field name\"][value=\"" + fieldName + "\"]').parent().parent().parent().parent().parent().find(\"i\").click()");
+
+        return new DeleteField();
     }
 
     public void clickSave()
