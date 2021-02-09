9f2bd7edd371415bbdc36e8c8ce23d182e6fbc1c
art1988 <art.khassanov@gmail.com>
2021-02-09 18:56:56 +0500

    FieldType: was added text value for each type

--- ParleyPro Autotests/src/main/java/constants/FieldType.java
+++ ParleyPro Autotests/src/main/java/constants/FieldType.java
@@ -2,13 +2,25 @@ package constants;
 
 public enum FieldType
 {
-    CHECKBOX,
-    DATE,
-    MULTI_SELECT,
-    NUMERIC,
-    DECIMAL,
-    RADIO_BUTTON,
-    SELECT,
-    TEXT,
-    TEXT_AREA
+    CHECKBOX("Checkbox"),
+    DATE("Date"),
+    MULTI_SELECT("Multi Select"),
+    NUMERIC("Numeric"),
+    DECIMAL("Decimal"),
+    RADIO_BUTTON("Radio Button"),
+    SELECT("Select"),
+    TEXT("Text"),
+    TEXT_AREA("Textarea");
+
+    private String type;
+
+    FieldType(String type)
+    {
+        this.type = type;
+    }
+
+    public String getFieldType()
+    {
+        return type;
+    }
 }
