a6b15e7aefff3d25ce66041a5b5f90fe0278263d
art1988 <art.khassanov@gmail.com>
2021-02-16 18:01:34 +0500

    FieldsRelations: were added methods addNewParentField, addRelatedField

--- ParleyPro Autotests/src/main/java/pages/administration/fields_breadcrumb/FieldsRelations.java
+++ ParleyPro Autotests/src/main/java/pages/administration/fields_breadcrumb/FieldsRelations.java
@@ -4,21 +4,43 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import forms.add.AddNewParentField;
+import forms.add.AddRelatedField;
+import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class FieldsRelations
 {
+    private SelenideElement addNewParentField = $("#add-new-parent-field");
+
+    private static Logger logger = Logger.getLogger(FieldsRelations.class);
+
     public FieldsRelations()
     {
-        $$(".admin-fields__title").shouldHave(CollectionCondition.size(3))
+        $$(".admin-fields__title").shouldHave(CollectionCondition.size(2))
                 .shouldHave(CollectionCondition.exactTexts("Summary", "Contract Request"));
     }
 
-    /*
     public AddNewParentField addNewParentField()
     {
-        Selenide.executeJavaScript("");
-    }*/
+        addNewParentField.click();
+
+        logger.info("Add new parent field link was clicked");
+
+        return new AddNewParentField();
+    }
+
+    /**
+     * Click by 'Add related field' link for given field name
+     * @param fieldName
+     */
+    public AddRelatedField addRelatedField(String fieldName)
+    {
+        Selenide.executeJavaScript("$('.admin-fields-section div:contains(\"" + fieldName + "\")').find(\"i\").click()");
+
+        logger.info("+ Add related field was clicked for: " + fieldName);
+
+        return new AddRelatedField();
+    }
 }
