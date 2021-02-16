6382241f6963589214a406a30701633a32ea5e0b
art1988 <art.khassanov@gmail.com>
2021-02-16 18:02:10 +0500

    finished with test AT-80

--- ParleyPro Autotests/src/test/java/tests/regression/at80/AddRequestFieldsAndAddRelatedField.java
+++ ParleyPro Autotests/src/test/java/tests/regression/at80/AddRequestFieldsAndAddRelatedField.java
@@ -2,22 +2,30 @@ package tests.regression.at80;
 
 import com.codeborne.selenide.Condition;
 import constants.FieldType;
+import forms.add.AddNewParentField;
+import forms.add.AddRelatedField;
 import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
-import pages.administration.fields_breadcrumb.ContractFields;
 import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
 import pages.administration.fields_breadcrumb.FieldsRelations;
+import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Listeners({ScreenShotOnFailListener.class})
 public class AddRequestFieldsAndAddRelatedField
 {
     private Fields fieldsTab;
+    private static Logger logger = Logger.getLogger(AddRequestFieldsAndAddRelatedField.class);
 
     @Test(priority = 1)
-    @Description("This test goes to Administration -> Fields, adds 2 new fiends under Contract Request section")
+    @Description("This test goes to Administration -> Fields, adds 2 new fields under Contract Request section, Saves and click Next")
     public void addRequestField()
     {
         fieldsTab = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
@@ -44,10 +52,26 @@ public class AddRequestFieldsAndAddRelatedField
     }
 
     @Test(priority = 2)
+    @Description("This test goes to FIELD RELATIONS and adds new parent field")
     public void addParentField()
     {
         FieldsRelations fieldsRelations = fieldsTab.clickFieldsRelations();
 
-        //fieldsRelations.addNewParentField();
+        AddNewParentField addNewParentFieldForm = fieldsRelations.addNewParentField();
+        addNewParentFieldForm.selectParentField("Field1");
+        addNewParentFieldForm.clickCreate();
+
+        AddRelatedField addRelatedFieldForm = fieldsRelations.addRelatedField("Field1");
+        addRelatedFieldForm.selectValueForField("Field1", "Value1");
+        addRelatedFieldForm.selectFields("Field2");
+        addRelatedFieldForm.clickMakeRelated();
+
+        fieldsTab.clickSave();
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+
+        logger.info("Assert that related field was added...");
+        Assert.assertEquals($("div[class^='styles__field_value_header']").getText(), "Fields related to \nValue1\n value\n1\narrow_drop_down");
+
+        Screenshoter.makeScreenshot();
     }
 }
---
+++ ParleyPro Autotests/src/test/java/tests/regression/at80/RemoveFieldAndCheckUI.java
@@ -0,0 +1,65 @@
+package tests.regression.at80;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
+import pages.administration.fields_breadcrumb.FieldsRelations;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class RemoveFieldAndCheckUI
+{
+    private Fields fieldsTab;
+    private static Logger logger = Logger.getLogger(RemoveFieldAndCheckUI.class);
+
+    @Test(priority = 1)
+    @Description("This test removes Field2 field, click Next and verifies that no grey screen on FIELD RELATIONS")
+    public void removeFieldAndCheckUI() throws InterruptedException
+    {
+        fieldsTab = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFields = fieldsTab.clickContractFields();
+
+        // Scroll to bottom of page
+        Selenide.executeJavaScript("$('.admin-fields__title:contains(\"Contract Request\")')[0].scrollIntoView({});");
+        contractFields.removeField("Field2").clickDelete();
+        fieldsTab.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+        Screenshoter.makeScreenshot();
+
+        fieldsTab.clickNext();
+        Thread.sleep(1_000);
+
+        logger.info("Assert that we still can see Field Relations tab...");
+        new FieldsRelations();
+        Assert.assertEquals($("div[class^='styles__field_value_header']").getText(), "Fields related to \nValue1\n value\n1\narrow_drop_down");
+        Assert.assertEquals($("div[class^='styles__field_head']").getText(), "Field1\nadd\nAdd related field");
+        Assert.assertEquals($(".admin-fields-relations__field-head").getText(), "Contract category\nadd\nAdd related field");
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 2)
+    public void cleanUp()
+    {
+        logger.info("Go back to CONTRACT FIELDS and remove Field1...");
+
+        ContractFields contractFields = fieldsTab.clickContractFields();
+
+        contractFields.removeField("Field1").clickDelete();
+        fieldsTab.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+    }
+}
