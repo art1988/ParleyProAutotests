806330df5581f9a93f5b62214fa7b027ae851cd1
art1988 <art.khassanov@gmail.com>
2021-02-18 17:58:15 +0500

    AT-77: finished ( on hold )

--- ParleyPro Autotests/src/test/java/tests/templates/at77/CreateTemplateAndPasteText.java
+++ ParleyPro Autotests/src/test/java/tests/templates/at77/CreateTemplateAndPasteText.java
@@ -1,13 +1,19 @@
 package tests.templates.at77;
 
 import constants.Const;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.EditTemplatePage;
 import pages.TemplatesPage;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
 
+@Listeners({ScreenShotOnFailListener.class})
 public class CreateTemplateAndPasteText
 {
+    private String templateName = "Template_AT-77_dummy";
     private String textToPaste = "\\n" +
             "<<[contract.getCustomFields().get(“Borrower5”)]>><</if>><<if[contract.getCustomFields().get(\"Borrower6\")!=null]>>\\n" +
             "\\n" +
@@ -43,14 +49,25 @@ public class CreateTemplateAndPasteText
             "<<[contract.getCustomFields().get(“Borrower15”)]>><</if>>\\n";
 
     @Test(priority = 1)
+    @Description("This test uploads template and paste text at the beginning")
     public void uploadDocAsTemplateAndPasteText() throws InterruptedException
     {
         TemplatesPage templatesPage = new DashboardPage().getSideBar().clickTemplates(true);
 
-        templatesPage.clickUploadTemplatesButton(Const.TEMPLATE_AT77 );
+        templatesPage.clickUploadTemplatesButton( Const.TEMPLATE_AT77 );
 
-        EditTemplatePage editTemplatePage = templatesPage.selectTemplate("Template_AT-77_dummy");
+        EditTemplatePage editTemplatePage = templatesPage.selectTemplate(templateName);
 
         editTemplatePage.addText(textToPaste);
+        Screenshoter.makeScreenshot();
+        editTemplatePage.clickPublishButton();
+    }
+
+    @Test(priority = 2)
+    @Description("This test reopens just added template and verifies that text was added")
+    public void reopenTemplate()
+    {
+        new DashboardPage().getSideBar().clickTemplates(false).selectTemplate(templateName);
+        // TODO: add verification after fixing of PAR-13426
     }
 }
