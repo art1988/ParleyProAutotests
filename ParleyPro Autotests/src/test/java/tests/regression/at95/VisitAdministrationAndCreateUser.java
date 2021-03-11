b5ad280851cedd6ede24c801b2d17d6c83e83d82
art1988 <art.khassanov@gmail.com>
2021-03-11 15:39:28 +0500

    finished with test AT-95; it was added to Regression suite

--- ParleyPro Autotests/src/test/java/tests/regression/at95/VisitAdministrationAndCreateUser.java
+++ ParleyPro Autotests/src/test/java/tests/regression/at95/VisitAdministrationAndCreateUser.java
@@ -1,23 +1,28 @@
 package tests.regression.at95;
 
 import forms.add.AddNewUser;
+import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.administration.ManageUsers;
+import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
+@Listeners({ScreenShotOnFailListener.class})
 public class VisitAdministrationAndCreateUser
 {
     private static Logger logger = Logger.getLogger(VisitAdministrationAndCreateUser.class);
 
     @Test(priority = 1)
+    @Description("This test clicks add new user in Administration, changes Region/Contract category/Department values and asserts that previous values re still the same.")
     public void visitAdministrationAndCreateUser()
     {
-        ManageUsers manageUsersPage = new DashboardPage().getSideBar().clickAdministration().clickManageUsersTab();
+        ManageUsers manageUsersTab = new DashboardPage().getSideBar().clickAdministration().clickManageUsersTab();
 
-        AddNewUser addNewUserForm = manageUsersPage.clickAddNewUser();
+        AddNewUser addNewUserForm = manageUsersTab.clickAddNewUser();
 
         addNewUserForm.setEmail("junk@junk.com");
         addNewUserForm.clickAddRole();
@@ -25,12 +30,34 @@ public class VisitAdministrationAndCreateUser
         addNewUserForm.setRegion("region2");
         addNewUserForm.setContractCategory("category1");
 
-        logger.info("Assert that 'Region' value is region2...");
+        logger.info("Assert that 'Region' value is still region2...");
         Assert.assertEquals(addNewUserForm.getRegion(), "region2", "Looks like that 'Region' value is wrong !");
 
-        logger.info("Assert that 'Contract category' is category1...");
+        logger.info("Assert that 'Contract category' is still category1...");
         Assert.assertEquals(addNewUserForm.getContractCategory(), "category1", "Looks like that 'Contract category' value is wrong !");
 
+        addNewUserForm.setDepartment("department2");
+
+        logger.info("Assert that other values are still the same...");
+        Assert.assertEquals(addNewUserForm.getRegion(), "region2", "Looks like that 'Region' value is wrong !");
+        Assert.assertEquals(addNewUserForm.getContractCategory(), "category1", "Looks like that 'Contract category' value is wrong !");
+
+        addNewUserForm.setRegion("region1");
+
+        logger.info("Assert that other values are still the same...");
+        Assert.assertEquals(addNewUserForm.getContractCategory(), "category1", "Looks like that 'Contract category' value is wrong !");
+        Assert.assertEquals(addNewUserForm.getDepartment(), "department2", "Looks like that 'Department' value is wrong !");
+        Assert.assertEquals(addNewUserForm.getRegion(), "region1", "Looks like that 'Region' value is wrong !");
+
+        addNewUserForm.setContractCategory("category2");
+
+        logger.info("Assert that other values are still the same...");
+        Assert.assertEquals(addNewUserForm.getRegion(), "region1", "Looks like that 'Region' value is wrong !");
+        Assert.assertEquals(addNewUserForm.getContractCategory(), "category2", "Looks like that 'Contract category' value is wrong !");
+        Assert.assertEquals(addNewUserForm.getDepartment(), "department2", "Looks like that 'Department' value is wrong !");
+
         Screenshoter.makeScreenshot();
+
+        addNewUserForm.clickCancel();
     }
 }
