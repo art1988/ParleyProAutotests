76f1fc6d9af40b7a83957a566c26d9bb53fca13f
art1988 <art.khassanov@gmail.com>
2021-03-15 14:23:38 +0500

    ContractInfo: was added additional spinner disappear condition

--- ParleyPro Autotests/src/main/java/pages/ContractInfo.java
+++ ParleyPro Autotests/src/main/java/pages/ContractInfo.java
@@ -23,6 +23,7 @@ public class ContractInfo
         $(".tab-menu__item.selected_yes").waitUntil(Condition.visible, 6_000).shouldHave(Condition.exactText("Post-execution"));
         $(".contract-execute-form").waitUntil(Condition.visible, 10_000);
         $(".spinner").waitUntil(Condition.disappear, 10_000);
+        $(".document__body .spinner").waitUntil(Condition.disappear, 15_000);
     }
 
     public void setSignatureDate()
