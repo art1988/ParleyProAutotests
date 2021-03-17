11a7ec5b340836bee579619dca4067160912d934
art1988 <art.khassanov@gmail.com>
2021-03-17 16:03:33 +0500

    were added listeners on fail

--- ParleyPro Autotests/src/test/java/tests/templates/at86/UploadPublishDownloadTemplate.java
+++ ParleyPro Autotests/src/test/java/tests/templates/at86/UploadPublishDownloadTemplate.java
@@ -8,9 +8,11 @@ import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.DashboardPage;
 import pages.TemplatesPage;
+import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
 import java.io.File;
@@ -21,6 +23,7 @@ import java.nio.file.Paths;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Listeners({ScreenShotOnFailListener.class})
 public class UploadPublishDownloadTemplate
 {
     private String templateName = "Template_AT-86_text_cut_off";
