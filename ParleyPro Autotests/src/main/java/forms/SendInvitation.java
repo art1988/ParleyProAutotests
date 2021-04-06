3655f49e2db42e7d76ccdf9691dda9cc64ff03c1
art1988 <art.khassanov@gmail.com>
2021-04-06 18:01:03 +0500

    SendInvitation: was completed message; was added clickStart method

--- ParleyPro Autotests/src/main/java/forms/SendInvitation.java
+++ ParleyPro Autotests/src/main/java/forms/SendInvitation.java
@@ -4,15 +4,16 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 
+import static com.codeborne.selenide.Condition.or;
 import static com.codeborne.selenide.Selenide.$;
 
 /**
- * Represents form that appears after clicking on SEND INVITE button
+ * Represents form that appears after clicking on SEND INVITE button.
+ * Or by clicking MAKE EXTERNAL in Manage Discussions form.
  */
 public class SendInvitation
 {
-    private SelenideElement nextButton = $(".button.btn-common.btn.btn-primary");
-
+    private SelenideElement nextButton  = $(".button.btn-common.btn.btn-primary");
 
     private static Logger logger = Logger.getLogger(SendInvitation.class);
 
@@ -22,7 +23,8 @@ public class SendInvitation
                 .shouldHave(Condition.exactText("You are about to send invitation to negotiate for contract \"" + contractName + "\"."));
 
         $(".share-documents__message").waitUntil(Condition.visible, 7_000)
-                .shouldHave(Condition.exactText("Once sent, selected documents will be visible to the Counterparty"));
+                .shouldHave(or("message on popup", Condition.exactText("Once sent, selected documents will be visible to the Counterparty"),
+                                                         Condition.exactText("Once sent, selected documents will be emailed to the Counterparty")));
     }
 
     public EmailWillBeSentToTheCounterparty clickNext(boolean isClassic)
@@ -32,5 +34,12 @@ public class SendInvitation
         logger.info("NEXT button was clicked...");
 
         return new EmailWillBeSentToTheCounterparty(isClassic);
+    }
+
+    public void clickStart()
+    {
+        nextButton.click(); // START button has the same css locator
+
+        logger.info("START button was clicked...");
     }
 }
