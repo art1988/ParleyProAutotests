8a31a4e504ad22bc4b38b0c9f6ca03d3cd82facf
art1988 <art.khassanov@gmail.com>
2021-04-09 15:44:45 +0500

    finished with test AT-84; was added to Fields suite

---
+++ ParleyPro Autotests/src/test/java/tests/fields/at84/RemoveFields.java
@@ -0,0 +1,39 @@
+package tests.fields.at84;
+
+import com.codeborne.selenide.Condition;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class RemoveFields
+{
+    private static Logger logger = Logger.getLogger(RemoveFields.class);
+
+    @Test(priority = 1)
+    public void removeCreatedFields()
+    {
+        Fields fieldsPage = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFields = fieldsPage.clickContractFields();
+
+        contractFields.removeField("Field1").clickDelete();
+        contractFields.removeField("Field2").clickDelete();
+        contractFields.removeField("Field3").clickDelete();
+
+        fieldsPage.clickSave();
+
+        logger.info("Assert that fields were completely removed...");
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+        Assert.assertFalse($("input[value='Field1']").isDisplayed(), "Looks like that Field1 wasn't deleted !!!");
+        Assert.assertFalse($("input[value='Field2']").isDisplayed(), "Looks like that Field2 wasn't deleted !!!");
+        Assert.assertFalse($("input[value='Field3']").isDisplayed(), "Looks like that Field3 wasn't deleted !!!");
+    }
+}
--- ParleyPro Autotests/src/test/java/tests/fields/at84/VerifyFields.java
+++ ParleyPro Autotests/src/test/java/tests/fields/at84/VerifyFields.java
@@ -1,15 +1,56 @@
 package tests.fields.at84;
 
+import com.codeborne.selenide.Selenide;
+import constants.FieldType;
+import forms.ContractInformation;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import pages.DashboardPage;
 import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
 
 @Listeners({ScreenShotOnFailListener.class})
 public class VerifyFields
 {
-    @Test
-    public void verifyFields()
-    {
+    private static Logger logger = Logger.getLogger(VerifyFields.class);
 
+    @Test
+    public void verifyFields() throws InterruptedException
+    {
+        ContractInformation contractInformationForm = new DashboardPage().getSideBar()
+                                                                         .clickInProgressContracts(true)
+                                                                         .clickNewContractButton();
+
+        contractInformationForm.setValueForCustomField("Field1", FieldType.SELECT, "v1");
+        expandFieldAndCheck("Field2", "v11");
+        expandFieldAndCheck("Field3", "v111");
+
+        // Close form...
+        contractInformationForm.clickCancel();
+
+        //  ...and open again
+        contractInformationForm = new DashboardPage().getSideBar()
+                                                     .clickInProgressContracts(true)
+                                                     .clickNewContractButton();
+
+        contractInformationForm.setValueForCustomField("Field1", FieldType.SELECT, "v2");
+        expandFieldAndCheck("Field2", "v22");
+        expandFieldAndCheck("Field3", "v222");
+
+        contractInformationForm.clickCancel();
+    }
+
+    // Helper method that expands dropdown by the name 'fieldName' and asserts that only 'valueToCheck' is present in the list
+    private void expandFieldAndCheck(String fieldName, String valueToCheck) throws InterruptedException
+    {
+        logger.info("Expand '" + fieldName + "' and check that only '" + valueToCheck + "' is available...");
+        Selenide.executeJavaScript("$('.input__label:contains(\"" + fieldName + "\")').parent().find('input').attr('id', '" + fieldName + "')"); // set id dynamically for Field2
+        $("#" + fieldName).click(); // expand dropdown
+        Thread.sleep(500);
+        Assert.assertEquals(Selenide.executeJavaScript("return $('.dropdown-menu:visible').find(\".multi-select__option\").text()"), valueToCheck,
+                "Looks like that " + fieldName + " has more than '" + valueToCheck + "' values to select !!!");
     }
 }
