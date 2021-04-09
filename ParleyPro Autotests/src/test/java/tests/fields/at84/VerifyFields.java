0b2c8bef1b876de1ac7ff0b87300e35d92df9ed9
art1988 <art.khassanov@gmail.com>
2021-04-09 16:03:13 +0500

    was added test description for report for AT-84 test

--- ParleyPro Autotests/src/test/java/tests/fields/at84/VerifyFields.java
+++ ParleyPro Autotests/src/test/java/tests/fields/at84/VerifyFields.java
@@ -3,6 +3,7 @@ package tests.fields.at84;
 import com.codeborne.selenide.Selenide;
 import constants.FieldType;
 import forms.ContractInformation;
+import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
@@ -19,6 +20,7 @@ public class VerifyFields
     private static Logger logger = Logger.getLogger(VerifyFields.class);
 
     @Test
+    @Description("This test set value v1/v2 for Field1 and checks that Field2 has only v11/v22 and Field3 has only v111/v222. ")
     public void verifyFields() throws InterruptedException
     {
         ContractInformation contractInformationForm = new DashboardPage().getSideBar()
@@ -48,7 +50,7 @@ public class VerifyFields
     private void expandFieldAndCheck(String fieldName, String valueToCheck) throws InterruptedException
     {
         logger.info("Expand '" + fieldName + "' and check that only '" + valueToCheck + "' is available...");
-        Selenide.executeJavaScript("$('.input__label:contains(\"" + fieldName + "\")').parent().find('input').attr('id', '" + fieldName + "')"); // set id dynamically for Field2
+        Selenide.executeJavaScript("$('.input__label:contains(\"" + fieldName + "\")').parent().find('input').attr('id', '" + fieldName + "')"); // set id dynamically for fieldName
         $("#" + fieldName).click(); // expand dropdown
         Thread.sleep(500);
         Assert.assertEquals(Selenide.executeJavaScript("return $('.dropdown-menu:visible').find(\".multi-select__option\").text()"), valueToCheck,
