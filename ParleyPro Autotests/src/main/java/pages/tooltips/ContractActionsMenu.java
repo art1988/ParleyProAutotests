2ecf883a39f9bb55d0c22fb172d829ec51153ea6
art1988 <art.khassanov@gmail.com>
2021-04-19 18:54:08 +0500

    was added clickAmendContract method

--- ParleyPro Autotests/src/main/java/pages/tooltips/ContractActionsMenu.java
+++ ParleyPro Autotests/src/main/java/pages/tooltips/ContractActionsMenu.java
@@ -2,13 +2,15 @@ package pages.tooltips;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import forms.ContractInformation;
 import forms.delete.DeleteContract;
 import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
 
 /**
- * Class that represents menu with 'Cancel' and 'Delete' contract options
+ * Class that represents menu with 'Cancel' and 'Delete' contract options.
+ * In executed status additional menu items were added: 'Amend contract' and 'Terminate contract'.
  */
 public class ContractActionsMenu
 {
@@ -32,5 +34,14 @@ public class ContractActionsMenu
         logger.info("Delete contract was clicked...");
 
         return new DeleteContract(contractName);
+    }
+
+    public ContractInformation clickAmendContract()
+    {
+        Selenide.executeJavaScript("$('.contract-header__menu .dropdown-menu.dropdown-menu-right a:contains(\"Amend\")')[0].click()");
+
+        logger.info("Amend contract was clicked...");
+
+        return new ContractInformation();
     }
 }
