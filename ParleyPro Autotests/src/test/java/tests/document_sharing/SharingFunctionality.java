d17d1339d71b9ac485d52b5a2d92de65bede7e3f
art1988 <art.khassanov@gmail.com>
2021-04-27 19:50:34 +0500

    was increased time of email delivery

--- ParleyPro Autotests/src/test/java/tests/document_sharing/SharingFunctionality.java
+++ ParleyPro Autotests/src/test/java/tests/document_sharing/SharingFunctionality.java
@@ -64,8 +64,8 @@ public class SharingFunctionality
         $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Email has been successfully sent."));
         $(".notification-stack").waitUntil(Condition.disappear, 15_000);
 
-        logger.info("Waiting for 30 seconds to make sure that email has been delivered...");
-        Thread.sleep(30_000);
+        logger.info("Waiting for 40 seconds to make sure that email has been delivered...");
+        Thread.sleep(40_000);
         Assert.assertTrue(EmailChecker.assertEmailBySubject(host, username, password, "Contract \"Contract DOCUMENT Sharing\": document review request"),
                 "Email with subject: Contract \"Contract DOCUMENT Sharing\": document review request was not found !!!");
 
