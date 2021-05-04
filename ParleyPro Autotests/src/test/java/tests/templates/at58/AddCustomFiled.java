5beedd7c38abe3642e9e8a9a2af6ef9eaf3e93c8
art1988 <art.khassanov@gmail.com>
2021-05-04 23:10:41 +0500

    AddCustomFiled in AT-58, was added refresh page to remove long lasting notifications after adding of custom field; CI: run only Templates - DEBUG

--- ParleyPro Autotests/src/test/java/tests/templates/at58/AddCustomFiled.java
+++ ParleyPro Autotests/src/test/java/tests/templates/at58/AddCustomFiled.java
@@ -1,7 +1,10 @@
 package tests.templates.at58;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import constants.FieldType;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.administration.Fields;
@@ -25,5 +28,10 @@ public class AddCustomFiled
         $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
 
         Screenshoter.makeScreenshot();
+
+        Selenide.refresh();
+
+        new WebDriverWait(WebDriverRunner.getWebDriver(), 15).until(webDriver ->
+                Selenide.executeJavaScript("return document.readyState").equals("complete"));
     }
 }
