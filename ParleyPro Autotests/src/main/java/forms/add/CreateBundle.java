cc5bba377be6d6a0a8c99f649eb7d22d62499fa9
art1988 <art.khassanov@gmail.com>
2021-05-07 19:25:07 +0500

    were added setters and other methods

--- ParleyPro Autotests/src/main/java/forms/add/CreateBundle.java
+++ ParleyPro Autotests/src/main/java/forms/add/CreateBundle.java
@@ -1,8 +1,10 @@
 package forms.add;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -14,6 +16,7 @@ public class CreateBundle
 
     private SelenideElement cancelButton = $("._button.scheme_gray.size_lg");
     private SelenideElement nextButton   = $("._button.scheme_blue.size_lg");
+    private SelenideElement backArrow    = $(".manage-users-new__back");
 
 
     private static Logger logger = Logger.getLogger(CreateBundle.class);
@@ -57,5 +60,67 @@ public class CreateBundle
         }
 
         selectTemplatesDropdown.click(); // collapse dropdown
+    }
+
+    /**
+     * Removes template by name ( CONTAINS ! ) by clicking x
+     * @param templateName name of the template to be removed
+     * @return
+     */
+    public CreateBundle removeTemplate(String templateName)
+    {
+        Selenide.executeJavaScript("$('.template-bundle__item-name:contains(\"" + templateName + "\")').parent().find('.template-bundle__item-remove').click()");
+
+        logger.info("Template " + templateName + " was removed...");
+
+        return this;
+    }
+
+    public CreateBundle clickNext()
+    {
+        nextButton.click();
+
+        logger.info("NEXT button was clicked.");
+
+        return this;
+    }
+
+    public CreateBundle clickBack()
+    {
+        backArrow.waitUntil(Condition.visible, 7_000).click();
+
+        logger.info("< BACK button was clicked.");
+
+        return this;
+    }
+
+    public CreateBundle setRegion(String region)
+    {
+        $("#template_region div").waitUntil(Condition.visible, 7_000).click(); // expand dropdown
+        $(".Select-menu-outer").waitUntil(Condition.appear, 7_000);
+        WebElement item = Selenide.executeJavaScript("return $('.Select-menu-outer .Select-option:contains(\"" + region + "\")')[0]");
+        $(item).click();
+
+        return this;
+    }
+
+    public CreateBundle setDepartment(String department)
+    {
+        $("#template_department div").waitUntil(Condition.visible, 7_000).click(); // expand dropdown
+        $(".Select-menu-outer").waitUntil(Condition.appear, 7_000);
+        WebElement item = Selenide.executeJavaScript("return $('.Select-menu-outer .Select-option:contains(\"" + department + "\")')[0]");
+        $(item).click();
+
+        return this;
+    }
+
+    public CreateBundle setCategory(String category)
+    {
+        $("#template_category div").waitUntil(Condition.visible, 7_000).click(); // expand dropdown
+        $(".Select-menu-outer").waitUntil(Condition.appear, 7_000);
+        WebElement item = Selenide.executeJavaScript("return $('.Select-menu-outer .Select-option:contains(\"" + category + "\")')[0]");
+        $(item).click();
+
+        return this;
     }
 }
