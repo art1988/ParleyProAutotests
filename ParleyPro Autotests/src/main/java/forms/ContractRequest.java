08a4113b7195bcbf03fcf5fc0cbce0eea82580ff
art1988 <art.khassanov@gmail.com>
2021-05-14 18:44:56 +0500

    ContractRequest form: were added setters

--- ParleyPro Autotests/src/main/java/forms/ContractRequest.java
+++ ParleyPro Autotests/src/main/java/forms/ContractRequest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -33,6 +34,30 @@ public class ContractRequest
         Selenide.executeJavaScript("$('.row label:contains(\"" + fieldName + "\")').parent().find(\"input\").attr('id', '" + fieldName + "')");
         $("#" + fieldName).sendKeys(value);
         $("#" + fieldName).pressEnter();
+    }
+
+    /**
+     * General method for setting value for select field
+     * @param fieldName
+     * @param value
+     */
+    public void setValueForSelect(String fieldName, String value)
+    {
+        WebElement selectElement = Selenide.executeJavaScript("return $('.input__label-title:contains(\"" + fieldName + "\")').parent().next().find(\"input\")[0]");
+        $(selectElement).sendKeys(value);
+    }
+
+    /**
+     * Set _one_ contract type
+     * @param type
+     */
+    public void setContractType(String type)
+    {
+        $("input[data-id=\"contracttype\"]").click(); // open Contract type dropdown
+
+        Selenide.executeJavaScript("$('span:contains(\"Contract type\")').parent().parent().next().find(\"label:contains('" + type + "')\").click()");
+        $(".modal-content .spinner").waitUntil(Condition.disappear, 7_000);
+        $("input[data-id=\"contracttype\"]").click(); // click by input to collapse dropdown
     }
 
     public void clickCancel()
