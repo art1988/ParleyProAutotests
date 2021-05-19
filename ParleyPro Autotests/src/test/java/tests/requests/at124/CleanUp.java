b76a022e6b6f913b2a5d8a29a414171bc10c796d
art1988 <art.khassanov@gmail.com>
2021-05-19 19:05:55 +0500

    finished with AT-124 scenario

---
+++ ParleyPro Autotests/src/test/java/tests/requests/at124/CleanUp.java
@@ -0,0 +1,66 @@
+package tests.requests.at124;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class CleanUp
+{
+    private static Logger logger = Logger.getLogger(CleanUp.class);
+
+    @Test(priority = 1)
+    public void removeWorkflow()
+    {
+        new DashboardPage().getSideBar()
+                           .clickAdministration()
+                           .clickWorkflowsTab()
+                           .clickActionMenu("requests")
+                           .clickDelete()
+                           .clickDelete();
+
+        try
+        {
+            Thread.sleep(1_000);
+        }
+        catch (InterruptedException e)
+        {
+            logger.error("InterruptedException", e);
+        }
+
+        logger.info("Assert that there is no contract routing workflow anymore...");
+        Assert.assertTrue(Selenide.executeJavaScript("return $('.workflows-list .workflows-list__row').length === 1"));
+    }
+
+    @Test(priority = 2)
+    public void removeFields()
+    {
+        Fields fieldsPage = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFields = fieldsPage.clickContractFields();
+
+        contractFields.removeField("Contracting region", "Contract Request").clickDelete();
+        contractFields.removeField("Contracting country", "Contract Request").clickDelete();
+        contractFields.removeField("Contract entity", "Contract Request").clickDelete();
+        contractFields.removeField("Contracting department", "Contract Request").clickDelete();
+        contractFields.removeField("Contract category", "Contract Request").clickDelete();
+        contractFields.removeField("Contract type", "Contract Request").clickDelete();
+        contractFields.removeField("Contract value", "Contract Request").clickDelete();
+
+        fieldsPage.clickSave();
+
+        logger.info("Assert that fields were completely removed...");
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+        Assert.assertTrue(Selenide.executeJavaScript("return $('.admin-fields__title:contains(\"Contract Request\")').parent().find(\".admin-fields-staticfield.js-item\").length === 0"),
+                "Looks like that not all fields were removed from Contract Request !!!");
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/requests/at124/LoginBackAsCNAndCheck.java
@@ -0,0 +1,106 @@
+package tests.requests.at124;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import constants.Const;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class LoginBackAsCNAndCheck
+{
+    private static Logger logger = Logger.getLogger(LoginBackAsCNAndCheck.class);
+
+    @Test(priority = 1)
+    public void loginBackAsCNAndCheck() throws InterruptedException
+    {
+        LoginPage loginPage = new LoginPage();
+
+        // Login as my team CN
+        loginPage.setEmail( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_USER_CN_ROLE.getPassword() );
+
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+
+        logger.info("Making sure that contract with name 'Contract request' was added in In-progress contracts list...");
+        $$(".contracts-list__contract-name").shouldHave(CollectionCondition.size(1)).shouldHave(CollectionCondition.exactTexts("Contract request"));
+
+        logger.info("Assert that REQUEST label appeared...");
+        $(".label.label_theme_purple").shouldBe(Condition.visible).shouldHave(Condition.exactText("REQUEST"));
+
+        logger.info("Opening this request...");
+        dashboardPage.getSideBar().clickInProgressContracts(false).selectContract("Contract request");
+
+        new OpenedContract();
+        $(".contract-create__form .spinner").waitUntil(Condition.appear, 10_000);
+        $(".contract-create__form .spinner").waitUntil(Condition.disappear, 25_000);
+
+        logger.info("Making sure that 2 word documents become docs in contract and 1 jpeg img become attachment...");
+        $$(".document__header-rename").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.textsInAnyOrder("pramata", "AT-14"));
+        $(".supporting-documents__document-name").shouldBe(Condition.visible).shouldHave(Condition.exactText("IMG_JPEG.jpeg"));
+
+        logger.info("Checking request fields...");
+        Assert.assertEquals($("#contractingregion").val(), "region1", "[Request field] Contracting region is wrong !!!");
+        Assert.assertEquals($("#contractingcountry").val(), "country1", "[Request field] Contracting country is wrong !!!");
+        Assert.assertEquals($("#contractentity").val(), "entity1", "[Request field] Contract entity is wrong !!!");
+        Assert.assertEquals($("#contractingdepartment").val(), "department2", "[Request field] Contracting department is wrong !!!");
+        Assert.assertEquals($("#contractcategory").val(), "category2", "[Request field] Contract category is wrong !!!");
+        Assert.assertEquals($("input[data-id=\"contracttype\"]").val(), "type3", "[Request field] Contract type is wrong !!!");
+        Assert.assertEquals(Selenide.executeJavaScript("return $('span:contains(\"Contract value\")').parent().next().find(\"button\").text().trim()"), "EUR EUR", "Currency is wrong !!!");
+        Assert.assertEquals($("#contractvalue").val(), "4,500", "[Request field] Contract value is wrong !!!");
+
+        logger.info("Checking that summary fields values were populated from request fields...");
+        Assert.assertEquals($("#contractingRegion").val(), "region1", "[Summary field] Contracting region is wrong !!!");
+        Assert.assertEquals($("#contractingCountry").val(), "country1", "[Summary field] Contracting country is wrong !!!");
+        Assert.assertEquals($("#contractEntity").val(), "entity1", "[Summary field] Contract entity is wrong !!!");
+        Assert.assertEquals($("#ContractingDepartment").val(), "department2", "[Summary field] Contracting department is wrong !!!");
+        Assert.assertEquals($("#contractCategory").val(), "category2", "[Summary field] Contract category is wrong !!!");
+        Assert.assertEquals($("input[data-id=\"contractType\"]").val(), "type3", "[Summary field] Contract type is wrong !!!");
+        Assert.assertEquals($("#contractValue").val(), "4,500", "[Summary field] Contract value is wrong !!!");
+
+        Screenshoter.makeScreenshot();
+
+        logger.info("Clicking 'Show contract request' to hide...");
+        $(".contract-create-request-fields__tumbler div").click();
+        Thread.sleep(500);
+        Assert.assertFalse(Selenide.executeJavaScript("return $('.contract-create-request-fields__body').is(\":visible\")"));
+
+        logger.info("Clicking 'Show contract request' to show...");
+        $(".contract-create-request-fields__tumbler div").click();
+        Thread.sleep(500);
+        Assert.assertTrue(Selenide.executeJavaScript("return $('.contract-create-request-fields__body').is(\":visible\")"));
+    }
+
+    @Test(priority = 2)
+    public void fillFieldsAndSaveContract()
+    {
+        logger.info("Fill in CP and CCN fields and Save...");
+        $("#counterpartyOrganization").sendKeys("CounterpartyAT");
+        $(".Select-loading").waitUntil(Condition.disappear, 7_000); // wait until spinner for Counterparty organization will disappear
+        $("#counterpartyOrganization").pressEnter();
+        $(".select__loading").waitUntil(Condition.disappear, 7_000); // wait until spinner for CCN field will disappear
+
+        $("#counterpartyChiefNegotiator").sendKeys("arthur.khasanov+autotestcn@parleypro.com");
+        $(".button.btn.btn-common.btn-blue.btn.btn-default").click(); // click Save
+
+        logger.info("Checking that status was switched to Negotiate...");
+        $$(".lifecycle__item.active").first().waitUntil(Condition.exactText("NEGOTIATE\n(2)"), 10_000);
+        $$(".lifecycle__item.active").get(1).waitUntil(Condition.exactText("NEGOTIATE"), 10_000);
+        $$(".lifecycle__item.active").last().waitUntil(Condition.exactText("NEGOTIATE"), 10_000);
+        $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(3));
+
+        logger.info("Making sure that attachment was saved too...");
+        $(".supporting-documents__document-name").shouldBe(Condition.visible).shouldHave(Condition.exactText("IMG_JPEG.jpeg"));
+    }
+}
