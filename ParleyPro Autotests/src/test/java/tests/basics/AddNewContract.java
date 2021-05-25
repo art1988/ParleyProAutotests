2c9120b0b2a1af18e75494db5956ca7017c5bd8b
art1988 <art.khassanov@gmail.com>
2021-05-25 18:39:52 +0500

    was switched from hard assert to soft assert to be able to continue tests in case of fail

--- ParleyPro Autotests/src/test/java/tests/basics/AddNewContract.java
+++ ParleyPro Autotests/src/test/java/tests/basics/AddNewContract.java
@@ -2,13 +2,13 @@ package tests.basics;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import constants.Const;
 import forms.ContractInformation;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.InProgressContractsPage;
 import pages.OpenedContract;
 import utils.ScreenShotOnFailListener;
@@ -24,8 +24,8 @@ import static com.codeborne.selenide.Selenide.$;
 @Listeners({ScreenShotOnFailListener.class})
 public class AddNewContract
 {
+    private SoftAssert softAssert = new SoftAssert();
     private static Logger logger = Logger.getLogger(AddNewContract.class);
-
 
     @Test
     public void addNewContract() throws InterruptedException
@@ -57,35 +57,39 @@ public class AddNewContract
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM d, yyyy").withLocale(Locale.US);
 
-        logger.info("Open Contract Info and assert that all fields were saved...");
+        logger.info("Open [Contract Info] and assert that all fields were saved...");
         contractInformationForm = new OpenedContract().clickContractInfo();
 
-        Assert.assertEquals(contractInformationForm.getDueDate(), LocalDate.now().format(formatter));
-        Assert.assertEquals(contractInformationForm.getContractValue(), "18,000.00");
-        Assert.assertEquals(contractInformationForm.getContractRadioButtonSelection(), "Other");
-        Assert.assertEquals(contractInformationForm.getMyCompanyTemplateRadioButtonSelection(), "No");
-        //Assert.assertEquals(contractInformationForm.getCounterpartyOrganization(), "CounterpartyAT");  // !!! TEMPORARILY DISABLED !!!
-        Assert.assertEquals(contractInformationForm.getContractingRegion(), "region1");
-        Assert.assertEquals(contractInformationForm.getContractingCountry(), "country1");
-        Assert.assertEquals(contractInformationForm.getContractEntity(), "entity1");
-        Assert.assertEquals(contractInformationForm.getContractingDepartment(), "department1");
-        Assert.assertEquals(contractInformationForm.getContractCategory(), "category1");
-        Assert.assertEquals(contractInformationForm.getContractType(), "type1");
-        Assert.assertEquals(contractInformationForm.getChiefNegotiator(), Const.PREDEFINED_USER_CN_ROLE.getFirstName() + " " + Const.PREDEFINED_USER_CN_ROLE.getLastName() + " (" + Const.PREDEFINED_USER_CN_ROLE.getEmail() + ")");
+        softAssert.assertEquals(contractInformationForm.getDueDate(), LocalDate.now().format(formatter));
+        softAssert.assertEquals(contractInformationForm.getContractValue(), "18,000.00");
+        softAssert.assertEquals(contractInformationForm.getContractRadioButtonSelection(), "Other");
+        softAssert.assertEquals(contractInformationForm.getMyCompanyTemplateRadioButtonSelection(), "No");
+        softAssert.assertEquals(contractInformationForm.getCounterpartyOrganization(), "CounterpartyAT");
+        softAssert.assertEquals(contractInformationForm.getContractingRegion(), "region1");
+        softAssert.assertEquals(contractInformationForm.getContractingCountry(), "country1");
+        softAssert.assertEquals(contractInformationForm.getContractEntity(), "entity1");
+        softAssert.assertEquals(contractInformationForm.getContractingDepartment(), "department1");
+        softAssert.assertEquals(contractInformationForm.getContractCategory(), "category1");
+        softAssert.assertEquals(contractInformationForm.getContractType(), "type1");
+        softAssert.assertEquals(contractInformationForm.getChiefNegotiator(), Const.PREDEFINED_USER_CN_ROLE.getFirstName() + " " + Const.PREDEFINED_USER_CN_ROLE.getLastName() + " (" + Const.PREDEFINED_USER_CN_ROLE.getEmail() + ")");
         contractInformationForm.getTags().shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.textsInAnyOrder("Test", "One more tag"));
-        Assert.assertEquals(contractInformationForm.getNotes(), "These notes were added from autotest. Check me.");
+        softAssert.assertEquals(contractInformationForm.getNotes(), "These notes were added from autotest. Check me.");
 
+        Screenshoter.makeScreenshot();
         contractInformationForm.clickCancel();
 
         inProgressContractsPage.getSideBar().clickInProgressContracts(false);
 
-        logger.info("Assert that contract present in the list and 'Contract title', 'Chief Negotiator' and 'Stage' have correct values");
+        logger.info("Assert that Contract is present in the list and 'Contract title', 'Chief Negotiator' and 'Stage' have correct values");
 
         Waiter.smartWaitUntilVisible("$('.contracts-list__contract-name')");
-        Assert.assertEquals(Selenide.executeJavaScript("return $('.contracts-list__contract-name').text()"), "Contract lifecycle autotest");
+        Assert.assertEquals($(".contracts-list__contract-name").text(), "Contract lifecycle autotest");
 
         $("a .contracts-list__chief-negotiator-cell").shouldHave(Condition.exactText(Const.PREDEFINED_USER_CN_ROLE.getFirstName() + " " + Const.PREDEFINED_USER_CN_ROLE.getLastName()));
         $("a .contracts-list__cell-stage").shouldHave(Condition.exactText("Draft"));
+
+        logger.info("Perform assert of all fields on Contract Information form...");
+        softAssert.assertAll();
 
         Screenshoter.makeScreenshot();
     }
