c05a9664bfafe35a9ec84570d53d6a96fe9c0970
art1988 <art.khassanov@gmail.com>
2021-06-23 17:45:14 +0500

    finished with test AT-147

--- ParleyPro Autotests/src/test/java/tests/amendment/basics_at147/AmendExpiredContract.java
+++ ParleyPro Autotests/src/test/java/tests/amendment/basics_at147/AmendExpiredContract.java
@@ -3,19 +3,26 @@ package tests.amendment.basics_at147;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import constants.Const;
 import forms.CompleteSign;
+import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.AddDocuments;
 import pages.DashboardPage;
+import pages.ExecutedContractsPage;
 import pages.OpenedContract;
 import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
 import java.io.FileNotFoundException;
+import java.util.Set;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -34,7 +41,23 @@ public class AmendExpiredContract
                            .selectContract("Executed Expired");
 
         new OpenedContract().clickContractActionsMenu().clickAmendContract().clickSave();
-        new AddDocuments();
+        AddDocuments addDocuments = new AddDocuments();
+
+        softAssert.assertEquals(addDocuments.getContractTitle(), "Executed Expired-A", "Contract title is wrong !!!");
+        softAssert.assertAll();
+
+        logger.info("Making sure that Linked contracts icon was shown in header...");
+        $(".linked-contracts-label").shouldBe(Condition.visible).shouldHave(Condition.exactText("link 1"));
+
+        logger.info("Hover over Linked contracts icon...");
+        $(".linked-contracts-label").hover();
+        $(".spinner").waitUntil(Condition.disappear, 15_000);
+
+        $(".rc-tooltip-inner .js-linked-contracts-title").shouldHave(Condition.text("Linked contracts: 1"));
+        $(".rc-tooltip-inner .js-linked-contracts-head").shouldHave(Condition.text("Amendment to:\nExecuted Expired"));
+        $(".rc-tooltip-inner .js-linked-contracts-stage").shouldHave(Condition.text("Expired"));
+        $$($(".rc-tooltip-inner").findElements(By.cssSelector("div[class^='styles__date']"))).shouldHave(CollectionCondition.size(2))
+                .shouldHave(CollectionCondition.textsInAnyOrder("Effective date:\nMay 31, 2021", "Expiration date:\nMay 31, 2021"));
 
         Screenshoter.makeScreenshot();
     }
@@ -102,5 +125,53 @@ public class AmendExpiredContract
 
         $(".linked-contracts-label").hover();
         $(".spinner").waitUntil(Condition.disappear, 15_000);
+        $(".rc-tooltip-inner .js-linked-contracts-title").shouldHave(Condition.text("Linked contracts: 3"));
+        $$(".rc-tooltip-inner .js-linked-contracts-head").shouldHave(CollectionCondition.size(3))
+                .shouldHave(CollectionCondition.textsInAnyOrder("Amendment to:\nExecuted Signed-A", "Related to:\nExecuted Signed", "Related to:\nExecuted Signed-B"));
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 5)
+    @Description("This test goes to Executed contracts and hover link icon of 'Executed Signed-A' contract.")
+    public void hoverLinkIcon()
+    {
+        new DashboardPage().getSideBar().clickExecutedContracts(false);
+
+        logger.info("Hover over link icon of 'Executed Signed-A' contract and check that 'Amendment to', 'Amended by' and 'Related to' are correct...");
+
+        WebElement linkIcon = Selenide.executeJavaScript("return $('.contracts-list__contract-name:contains(\"Executed Signed-A\")').parent().next().find(\"span\")[0]");
+        $(linkIcon).hover();
+        $(".spinner").waitUntil(Condition.disappear, 15_000);
+
+        $(".rc-tooltip-inner .js-linked-contracts-title").shouldHave(Condition.text("Linked contracts: 3"));
+        $$(".rc-tooltip-inner .js-linked-contracts-head").shouldHave(CollectionCondition.size(3))
+                .shouldHave(CollectionCondition.textsInAnyOrder("Amendment to:\nExecuted Signed", "Amended by:\nExecuted Signed-A-A", "Related to:\nExecuted Signed-B"));
+        Screenshoter.makeScreenshot();
+
+        // This action opens contract in a new tab - so save prev. tab
+        WebDriver driver = WebDriverRunner.getWebDriver();
+        String currentHandle = driver.getWindowHandle();
+
+        logger.info("Click by link of 'Related to' contract...");
+        $((WebElement) Selenide.executeJavaScript("return $('.rc-tooltip-inner .js-linked-contracts-head:contains(\"Related\")').find(\"a\")[0]")).click();
+
+        Set<String> handles = driver.getWindowHandles();
+        for ( String actual : handles )
+        {
+            if ( !actual.equalsIgnoreCase(currentHandle) )
+            {
+                logger.info("Switching to opened tab...");
+                driver.switchTo().window(actual);
+                break;
+            }
+        }
+        AddDocuments addDocuments = new AddDocuments();
+
+        softAssert.assertEquals(addDocuments.getContractTitle(), "Executed Signed-B", "Contract title is wrong !!!");
+        softAssert.assertAll();
+
+        driver.close(); // close current tab
+        driver.switchTo().window(currentHandle); // switching to the original tab
     }
 }
---
+++ ParleyPro Autotests/src/test/java/tests/amendment/basics_at147/CleanUp.java
@@ -0,0 +1,54 @@
+package tests.amendment.basics_at147;
+
+import com.codeborne.selenide.Condition;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.ExecutedContractsPage;
+import pages.InProgressContractsPage;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class CleanUp
+{
+    @Test(priority = 1)
+    public void deleteAllContractsOnInProgressPage()
+    {
+        InProgressContractsPage inProgressContractsPage = new DashboardPage().getSideBar().clickInProgressContracts(false);
+
+        String[] contractsToDelete = {"Executed Signed-A-A", "Executed Signed-B", "Executed Expired-A", "New val for Executed Managed-A"};
+
+        for( int i = 0; i < contractsToDelete.length; i++ )
+        {
+            inProgressContractsPage.selectContract( contractsToDelete[i] );
+            new OpenedContract().clickContractActionsMenu().clickDeleteContract().clickDelete();
+            $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.text(" has been deleted."));
+            $(".notification-stack").waitUntil(Condition.disappear, 20_000);
+
+            if( i == contractsToDelete.length - 1 ) break;
+            inProgressContractsPage = new DashboardPage().getSideBar().clickInProgressContracts(false);
+        }
+    }
+
+    @Test(priority = 2)
+    public void deleteAllContractsOnExecutedPage()
+    {
+        ExecutedContractsPage executedContractsPage = new DashboardPage().getSideBar().clickExecutedContracts(false);
+
+        String[] contractsToDelete = {"Executed Signed-A", "Executed Expired", "Executed Managed", "Executed Signed"};
+
+        for( int i = 0; i < contractsToDelete.length; i++ )
+        {
+            executedContractsPage.selectContract( contractsToDelete[i] );
+            new OpenedContract().clickContractActionsMenu().clickDeleteContract().clickDelete();
+            $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.text(" has been deleted."));
+            $(".notification-stack").waitUntil(Condition.disappear, 20_000);
+
+            if( i == contractsToDelete.length - 1 ) break;
+            executedContractsPage = new DashboardPage().getSideBar().clickExecutedContracts(false);
+        }
+    }
+}
