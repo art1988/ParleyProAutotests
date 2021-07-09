3459c9c4dbc5bb503baa0fd098c784ac716dc5bb
art1988 <art.khassanov@gmail.com>
2021-07-09 18:41:50 +0500

    in-progress: AT-149

--- ParleyPro Autotests/src/test/java/tests/requests/at149/LoginAsRequesterAndCheck.java
+++ ParleyPro Autotests/src/test/java/tests/requests/at149/LoginAsRequesterAndCheck.java
@@ -1,18 +1,32 @@
 package tests.requests.at149;
 
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import constants.Const;
 import forms.ContractRequest;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import pages.ContractInfo;
 import pages.DashboardPage;
 import pages.LoginPage;
+import pages.OpenedContract;
 import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 @Listeners({ScreenShotOnFailListener.class})
 public class LoginAsRequesterAndCheck
 {
+    private static Logger logger = Logger.getLogger(LoginAsRequesterAndCheck.class);
+
     @Test
-    public void loginAsRequesterAndAddRequest()
+    public void loginAsRequesterAndAddRequest() throws InterruptedException
     {
         // Logout first
         LoginPage loginPage = new DashboardPage().getSideBar().logout();
@@ -24,5 +38,60 @@ public class LoginAsRequesterAndCheck
         DashboardPage dashboardPage = loginPage.clickSignIn();
 
         ContractRequest contractRequest = dashboardPage.getSideBar().clickInProgressContracts(true).clickNewRequestButton();
+        contractRequest.uploadDocuments(new File[]{Const.DOCUMENT_DISCUSSIONS_SAMPLE});
+        $(".upload-field__file-name").waitUntil(Condition.visible, 10_000).shouldHave(Condition.text("AT-14.docx"));
+
+        contractRequest.clickSubmitRequest();
+        $(".notification-stack").waitUntil(Condition.appear, 15_000).shouldHave(Condition.text("Your contract request has been submitted."));
+
+        dashboardPage.getSideBar().clickInProgressContracts(false).selectContract("Contract request");
+
+        ContractInfo contractInfo = new ContractInfo(true);
+
+        logger.info("Filling up all mandatory fields...");
+        contractInfo.setCounterpartyOrganization("CounterpartyAT");
+        contractInfo.setCounterpartyChiefNegotiator("arthur.khasanov+cpat@parleypro.com");
+        contractInfo.setContractingRegion("region1");
+        contractInfo.setContractingCountry("country1");
+        contractInfo.setContractEntity("entity1");
+        contractInfo.setContractingDepartment("department1");
+        contractInfo.setContractCategory("category1");
+        contractInfo.setContractType("type1");
+        contractInfo.clickSave();
+
+        logger.info("Checking that status was switched to NEGOTIATE...");
+        $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("NEGOTIATE\n(1)", "NEGOTIATE"));
+
+        new OpenedContract().clickByParagraph("Paragraph 1")
+                            .setText(" some new added text")
+                            .selectInternal()
+                            .clickPost();
+
+        logger.info("Assert that notification popup was shown...");
+        $(".notification-stack").waitUntil(Condition.appear, 25_000).shouldHave(Condition.text("Internal discussion"));
+        $(".notification-stack").waitUntil(Condition.disappear, 35_000);
+
+        logger.info("Assert that only one discussion was created...");
+        $$(".document__body-content .discussion-indicator").shouldHave(CollectionCondition.size(1));
+
+        Screenshoter.makeScreenshot();
+
+        logger.info("Making queued post...");
+        new OpenedContract().clickByDiscussionIcon("Paragraph 1").clickMakeQueued("some new");
+        $$(".discussion2-post").last().find(".active_queued span").shouldBe(Condition.visible).shouldHave(Condition.text("queued"));
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @AfterMethod
+    public void deleteContractRequest()
+    {
+        new DashboardPage().getSideBar().clickInProgressContracts(false).selectContract("Contract request");
+        new OpenedContract().clickContractActionsMenu().clickDeleteContract().clickDelete();
+
+        $(".notification-stack").waitUntil(Condition.visible, 20_000).shouldHave(Condition.exactText("Contract Contract request has been deleted."));
+
+        // Logout as requester
+        new DashboardPage().getSideBar().logout();
     }
 }
---
+++ ParleyPro Autotests/src/test/java/tests/requests/at149/RevertStateBack.java
@@ -0,0 +1,63 @@
+package tests.requests.at149;
+
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+import pages.administration.Fields;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class RevertStateBack
+{
+    private static Logger logger = Logger.getLogger(RevertStateBack.class);
+
+    @Test
+    @Description("This test logins back as my team CN and removes CN role for PREDEFINED_REQUESTER user; removes contract routing workflow and removes one request field.")
+    public void revertStateBack()
+    {
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.setEmail( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+        loginPage.setPassword( Const.PREDEFINED_USER_CN_ROLE.getPassword() );
+
+        DashboardPage dashboardPage = loginPage.clickSignIn();
+
+        logger.info("Restoring PREDEFINED_REQUESTER user...");
+        dashboardPage.getSideBar()
+                     .clickAdministration()
+                     .clickManageUsersTab()
+                     .clickActionMenu(Const.PREDEFINED_REQUESTER.getFirstName() + " " + Const.PREDEFINED_REQUESTER.getLastName())
+                     .clickEdit()
+                     .deleteRole("Chief Negotiator")
+                     .clickUpdateUser();
+
+        $(".notification-stack").waitUntil(Condition.appear, 15_000).shouldHave(Condition.text("updated successfully"));
+
+        //////
+        logger.info("Removing contract routing workflow...");
+        dashboardPage.getSideBar()
+                     .clickAdministration()
+                     .clickWorkflowsTab()
+                     .clickActionMenu("Routing workflow at-149")
+                     .clickDelete()
+                     .clickDelete();
+
+        $(".workflows-list__row:not(.type_header)").shouldBe(Condition.hidden);
+
+        //////
+        logger.info("Removing request field...");
+        Fields fieldsPage = dashboardPage.getSideBar().clickAdministration().clickFieldsTab();
+
+        fieldsPage.clickContractFields().removeField("request field").clickDelete();
+        fieldsPage.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+    }
+}
