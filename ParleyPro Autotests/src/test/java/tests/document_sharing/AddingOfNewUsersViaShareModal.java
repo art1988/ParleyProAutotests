2def47a5b491c7aebdf2d51e0f61cad41b2daaa1
art1988 <art.khassanov@gmail.com>
2021-07-14 16:18:45 +0500

    some more improvements for doc sharing

--- ParleyPro Autotests/src/test/java/tests/document_sharing/AddingOfNewUsersViaShareModal.java
+++ ParleyPro Autotests/src/test/java/tests/document_sharing/AddingOfNewUsersViaShareModal.java
@@ -87,6 +87,7 @@ public class AddingOfNewUsersViaShareModal
         for( int i = 0; i < $$(cssClassName + " .user").size(); i++ )
         {
             $$(cssClassName + " .user").get(i).hover();
+            Thread.sleep(1_000);
 
             if( i == 0 )
             {
@@ -94,7 +95,7 @@ public class AddingOfNewUsersViaShareModal
                 continue; // first hover over all user icons - nothing to check
             }
 
-            $(".rc-tooltip-inner .spinner").waitUntil(Condition.appear, 15_000);
+            $(".rc-tooltip-content").waitUntil(Condition.appear, 15_000);
             $(".rc-tooltip-inner .spinner").waitUntil(Condition.disappear, 25_000);
 
             String userRole = $(".contract-user__status").waitUntil(Condition.visible, 5_000).getText(),
