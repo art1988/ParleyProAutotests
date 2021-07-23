98a2953b064b30302ee04d44568e11fd233b5df0
art1988 <art.khassanov@gmail.com>
2021-07-23 15:18:54 +0500

    was added prod case

--- ParleyPro Autotests/src/test/java/tests/numbered_lists/at25/SupportedNumberingFormats.java
+++ ParleyPro Autotests/src/test/java/tests/numbered_lists/at25/SupportedNumberingFormats.java
@@ -311,9 +311,8 @@ public class SupportedNumberingFormats
         $(".notification-stack").waitUntil(Condition.appear, 15_000).shouldHave(Condition.text("Internal discussion"));
         $(".notification-stack").waitUntil(Condition.disappear, 15_000);
 
-        // if this is RC
-        // TODO: or prod too ?...
-        if( WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.net/rc") )
+        if( WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.net/rc") ||
+            WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.com") )
         {
             Assert.assertEquals(getWholeList(),"1.above first item,1.L0_Number_Point_1,2.L0_Number_Point_2,3.L0_Number_Point_B with sublevels," +
                     "3.1.L1_Number_Point_2_1,3.1.1.L2_Number_Point_C in sublevel,3.1.2.L2_Number_Point_2_2_1,3.1.2.1.L3_Number_Point_2_2_1_1," +
@@ -363,9 +362,8 @@ public class SupportedNumberingFormats
         $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText(" post has been successfully created."));
         $(".notification-stack").waitUntil(Condition.disappear, 15_000);
 
-        // if this is RC
-        // TODO: or prod too ?...
-        if( WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.net/rc") )
+        if( WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.net/rc") ||
+            WebDriverRunner.getWebDriver().getCurrentUrl().startsWith("https://qa-autotests.parleypro.com"))
         {
             Assert.assertEquals(getWholeList(), "1.above first item,1.L0_Number_Point_1,2.L0_Number_Point_2,3.L0_Number_Point_B with sublevels," +
                     "3.1.L1_Number_Point_2_1,3.1.1.L2_Number_Point_C in sublevel,3.1.2.L2_Number_Point_2_2_1,3.1.2.1.L3_Number_Point_2_2_1_1,3.1.2.1.1.L4_Number_Point_2_2_1_1_1," +
