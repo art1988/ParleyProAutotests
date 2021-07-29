ff27619504e33f7435aa595fe48c46fb7227f6a2
art1988 <art.khassanov@gmail.com>
2021-07-29 19:07:29 +0500

    AT-56: in-progress

---
+++ ParleyPro Autotests/src/test/java/tests/routing_workflow/at56/AddTeamAndTwoCustomFields.java
@@ -0,0 +1,72 @@
+package tests.routing_workflow.at56;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import constants.FieldType;
+import forms.add.AddMembers;
+import forms.add.AddNewTeam;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.Teams;
+import pages.administration.fields_breadcrumb.ContractFields;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddTeamAndTwoCustomFields
+{
+    @Test(priority = 1)
+    @Description("This test adds two custom fields in Summary: 'Enable Routing?' and 'Enable Approval?'")
+    public void addTwoCustomFields()
+    {
+        Fields fieldsTab = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFieldsPage = fieldsTab.clickContractFields();
+
+
+        contractFieldsPage.createNewFiled("Summary", "Enable Routing?", FieldType.SELECT, false);
+        contractFieldsPage.addValues("Enable Routing?", "Yes");
+        contractFieldsPage.addValues("Enable Routing?", "No");
+
+        contractFieldsPage.createNewFiled("Summary", "Enable Approval?", FieldType.SELECT, false);
+        contractFieldsPage.addValues("Enable Approval?", "Yes");
+        contractFieldsPage.addValues("Enable Approval?", "No");
+
+        fieldsTab.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 2)
+    @Description("This test adds team by the name Team123 with users 'vp1 ln', 'vp2' and 'vp3'.")
+    public void addTeam()
+    {
+        Teams teamsPage = new DashboardPage().getSideBar().clickAdministration().clickTeamsTab();
+
+        AddNewTeam addNewTeam = teamsPage.clickAddNewTeam();
+
+        addNewTeam.setTeamName("Team123");
+        AddMembers addMembers = addNewTeam.clickAddMembersButton();
+        addMembers.addParticipant("vp1");
+        addMembers.addParticipant("vp2");
+        addMembers.addParticipant("vp3");
+        addMembers.clickAdd();
+
+        addNewTeam.clickSave();
+        $(".notification-stack").waitUntil(Condition.visible, 15_000).shouldHave(Condition.exactText("Team added successfully."));
+
+        // assert that record was added
+        $$(".teams-list__row .teams-list__name").shouldHave(CollectionCondition.size(1))
+                                                           .shouldHave(CollectionCondition.exactTexts("Team123"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/routing_workflow/at56/AddWorkflows.java
@@ -0,0 +1,91 @@
+package tests.routing_workflow.at56;
+
+import com.codeborne.selenide.CollectionCondition;
+import constants.Const;
+import forms.workflows.ApprovalWorkflow;
+import forms.workflows.ContractRoutingWorkflow;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddWorkflows
+{
+    @Test(priority = 1)
+    @Description("This test adds routing workflow.")
+    public void addRoutingWorkflow()
+    {
+        ContractRoutingWorkflow routingWorkflow = new DashboardPage().getSideBar()
+                                                                     .clickAdministration()
+                                                                     .clickWorkflowsTab()
+                                                                     .clickAddNewWorkflow()
+                                                                     .clickContractRouting(false);
+
+        routingWorkflow.setName("Routing Workflow AT-56");
+
+        routingWorkflow.clickDraftToReview();
+        routingWorkflow.setDraftToReviewParticipant("Team123");
+        routingWorkflow.setDraftToReviewParticipant(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+
+        routingWorkflow.clickTextChanged();
+        routingWorkflow.setTextChangedParticipant("Team123");
+        routingWorkflow.setTextChangedParticipant(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+
+        routingWorkflow.clickSignatureDeclined();
+        routingWorkflow.setSignatureDeclinedParticipant("Team123");
+
+        routingWorkflow.clickUploadCounterpartyDocument();
+        routingWorkflow.setUploadCounterpartyDocumentParticipant("Team123");
+
+        routingWorkflow.clickContractRequested();
+        routingWorkflow.setContractRequestedParticipant(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+
+        routingWorkflow.addFieldAndValue("Enable Routing?", "Yes");
+
+
+        routingWorkflow.clickSave();
+
+        // assert that record was added
+        $$(".workflows-list__row:not(.type_header) .type_name").shouldHave(CollectionCondition.size(1))
+                                                                         .shouldHave(CollectionCondition.exactTexts("Routing Workflow AT-56"));
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 2)
+    @Description("This test adds approval workflow.")
+    public void addApprovalWorkflow()
+    {
+        ApprovalWorkflow approvalWorkflow = new DashboardPage().getSideBar()
+                                                               .clickAdministration()
+                                                               .clickWorkflowsTab()
+                                                               .clickAddNewWorkflow()
+                                                               .clickApproval(false);
+
+        approvalWorkflow.setName("Approval Workflow AT-56");
+
+        approvalWorkflow.clickPriorToNegotiate();
+        approvalWorkflow.setPriorToNegotiateParticipant(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+        approvalWorkflow.setPriorToNegotiateParticipant("Team123");
+
+        approvalWorkflow.clickPriorToSign();
+        approvalWorkflow.switchTumblerApprovalOrderOfPriorToSign();
+        approvalWorkflow.setPriorToSignParticipant(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+        approvalWorkflow.setPriorToSignParticipant("Team123");
+
+        approvalWorkflow.addFieldAndValue("Enable Approval?", "Yes");
+
+        approvalWorkflow.clickSave();
+
+        // assert that record was added
+        $$(".workflows-list__row:not(.type_header) .type_name").shouldHave(CollectionCondition.size(2))
+                                                                         .shouldHave(CollectionCondition.textsInAnyOrder("Routing Workflow AT-56", "Approval Workflow AT-56"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
