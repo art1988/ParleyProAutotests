96c52a04067fafa84bef7cdaaeb7912a79f11b07
art1988 <art.khassanov@gmail.com>
2021-08-20 15:40:19 +0500

    was added first doc for at-139

--- ParleyPro Autotests/src/main/java/constants/Const.java
+++ ParleyPro Autotests/src/main/java/constants/Const.java
@@ -96,6 +96,8 @@ public class Const
     public static final File   DOCUMENT_AT65_GLUE            = new File(System.getProperty("user.dir") + "/Documents/Dynatrace_Vendor_Addendum.docx");
     public static final File   DOCUMENT_AT65_GLUE_V2         = new File(System.getProperty("user.dir") + "/Documents/Dynatrace_Vendor_Addendum_v2.docx");
 
+    public static final File   DOC_MICROVENTION_AT139        = new File(System.getProperty("user.dir") + "/Documents/ForPAR-14078_AT139.docx");
+
     public static final File   IMG_JPEG_SAMPLE               = new File(System.getProperty("user.dir") + "/Documents/IMG_JPEG.jpeg");
 
     // Directories
