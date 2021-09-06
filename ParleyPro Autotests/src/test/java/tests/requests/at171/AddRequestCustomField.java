57f3940b950dde0ed1bb48b78cdeba79fd2d2e07
art1988 <art.khassanov@gmail.com>
2021-09-06 18:31:08 +0500

    AT-171: in-progress

---
+++ ParleyPro Autotests/src/test/java/tests/requests/at171/AddRequestCustomField.java
@@ -0,0 +1,31 @@
+package tests.requests.at171;
+
+import com.codeborne.selenide.Condition;
+import constants.FieldType;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
+import utils.ScreenShotOnFailListener;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddRequestCustomField
+{
+    @Test
+    public void addRequestCustomField()
+    {
+        Fields fields = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFields = fields.clickContractFields();
+
+        contractFields.createNewFiled("Contract Request", "reqField", FieldType.SELECT, false);
+        contractFields.addValues("reqField", "val1");
+        contractFields.addValues("reqField", "val2");
+        fields.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/requests/at171/AddRoutingWorkflow.java
@@ -0,0 +1,35 @@
+package tests.requests.at171;
+
+import com.codeborne.selenide.CollectionCondition;
+import constants.Const;
+import forms.workflows.ContractRoutingWorkflow;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddRoutingWorkflow
+{
+    @Test
+    public void addRoutingWorkflow()
+    {
+        ContractRoutingWorkflow contractRoutingWorkflow = new DashboardPage().getSideBar().clickAdministration()
+                .clickWorkflowsTab().clickAddNewWorkflow().clickContractRouting(false);
+
+        contractRoutingWorkflow.setName("Routing workflow for AT-171");
+        contractRoutingWorkflow.setCurrency("USD");
+        contractRoutingWorkflow.setMinValue("2314");
+        contractRoutingWorkflow.clickContractRequested();
+        contractRoutingWorkflow.setContractRequestedParticipant( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+        contractRoutingWorkflow.addFieldAndValue("reqField", "val1");
+        contractRoutingWorkflow.clickSave();
+
+        $$(".type_name").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("Name", "Routing workflow for AT-171"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
