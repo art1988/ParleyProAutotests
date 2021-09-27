77911de3d4ab72ebe44c0bd6571ac34a3fb0d4f7
art1988 <art.khassanov@gmail.com>
2021-09-27 18:26:34 +0500

    in-progress at-174

---
+++ ParleyPro Autotests/src/test/java/tests/requests/at174/AddFieldAndWorkflow.java
@@ -0,0 +1,70 @@
+package tests.requests.at174;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import constants.FieldType;
+import forms.workflows.ContractRoutingWorkflow;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.administration.Fields;
+import pages.administration.fields_breadcrumb.ContractFields;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddFieldAndWorkflow
+{
+    @Test(priority = 1)
+    public void addRequestField()
+    {
+        Fields fields = new DashboardPage().getSideBar().clickAdministration().clickFieldsTab();
+
+        ContractFields contractFields = fields.clickContractFields();
+
+        contractFields.createNewFiled("Contract Request", "FREQ1", FieldType.SELECT, false);
+
+        contractFields.addValues("FREQ1", "Yes");
+        contractFields.addValues("FREQ1", "No");
+        fields.clickSave();
+
+        $(".notification-stack").waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Contract fields have been saved."));
+    }
+
+    @Test(priority = 2)
+    public void addRoutingWorkflow()
+    {
+        ContractRoutingWorkflow contractRoutingWorkflow = new DashboardPage().getSideBar().clickAdministration()
+                .clickWorkflowsTab().clickAddNewWorkflow().clickContractRouting(false);
+
+        contractRoutingWorkflow.setName("AT-174 Wfl");
+        contractRoutingWorkflow.setCurrency("USD");
+        contractRoutingWorkflow.setMinValue("5001");
+
+        contractRoutingWorkflow.clickDraftToReview();
+        contractRoutingWorkflow.setDraftToReviewParticipant( Const.USER_GREG.getEmail() );
+
+        contractRoutingWorkflow.clickTextChanged();
+        contractRoutingWorkflow.setTextChangedParticipant( Const.USER_MARY.getEmail() );
+
+        contractRoutingWorkflow.clickSignatureDeclined();
+        contractRoutingWorkflow.setSignatureDeclinedParticipant( Const.USER_FELIX.getEmail() );
+
+        contractRoutingWorkflow.clickUploadCounterpartyDocument();
+        contractRoutingWorkflow.setUploadCounterpartyDocumentParticipant( Const.PREDEFINED_INTERNAL_USER_1.getEmail() );
+
+        contractRoutingWorkflow.clickContractRequested();
+        contractRoutingWorkflow.setContractRequestedParticipant( Const.PREDEFINED_USER_CN_ROLE.getEmail() );
+
+        contractRoutingWorkflow.addFieldAndValue("FREQ1", "Yes");
+        contractRoutingWorkflow.clickSave();
+
+        $$(".type_name").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("Name", "AT-174 Wfl"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/requests/at174/CreateContractUploadDocAndCheck.java
@@ -0,0 +1,55 @@
+package tests.requests.at174;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import forms.ContractInformation;
+import forms.StartReview;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.AddDocuments;
+import pages.DashboardPage;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class CreateContractUploadDocAndCheck
+{
+    private static Logger logger = Logger.getLogger(CreateContractUploadDocAndCheck.class);
+
+    @Test
+    public void createContractUploadDocAndCheck()
+    {
+        ContractInformation contractInformation = new DashboardPage().getSideBar().clickInProgressContracts(true).clickNewContractButton();
+
+        contractInformation.setContractTitle("CTR for at-174");
+        contractInformation.setContractValue("5002");
+        contractInformation.setCounterpartyOrganization("CounterpartyAT");
+        contractInformation.setCounterpartyChiefNegotiator( Const.PREDEFINED_CCN.getEmail() );
+        contractInformation.setContractingRegion("region1");
+        contractInformation.setContractingCountry("country1");
+        contractInformation.setContractEntity("entity1");
+        contractInformation.setContractingDepartment("department1");
+        contractInformation.setContractCategory("category1");
+        contractInformation.setContractType("type1");
+        contractInformation.clickSave();
+
+        new AddDocuments().clickUploadMyTeamDocuments( Const.DOCUMENT_LIFECYCLE_SAMPLE );
+
+        new OpenedContract().switchDocumentToReview("pramata").clickStart();
+
+        // Wait until status was changed to REVIEW
+        $$(".lifecycle__item.active").first().waitUntil(Condition.exactText("REVIEW\n(1)"), 17_000);
+        $$(".lifecycle__item.active").last().waitUntil(Condition.exactText("REVIEW"), 17_000);
+
+        logger.info("Checking that users weren't triggered...");
+        $$(".contract-header-users .user").shouldHave(CollectionCondition.size(1)).shouldHave(CollectionCondition.exactTexts("AL"));
+        $$(".document__score .user").shouldHave(CollectionCondition.size(1)).shouldHave(CollectionCondition.exactTexts("AL"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
