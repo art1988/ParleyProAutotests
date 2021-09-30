7b49940e02969a4ca80b95b567e7617f3d067422
art1988 <art.khassanov@gmail.com>
2021-09-30 17:35:00 +0500

    increased general smart delay up to 20 sec

--- ParleyPro Autotests/src/test/java/tests/LoginToDashboard.java
+++ ParleyPro Autotests/src/test/java/tests/LoginToDashboard.java
@@ -27,7 +27,7 @@ public class LoginToDashboard extends LoginBase
         Configuration.proxyEnabled   = true;
         Configuration.fileDownload   = FileDownloadMode.PROXY;
         Configuration.browserSize = "1920x1080";
-        Configuration.timeout = 10_000;
+        Configuration.timeout = 20_000;
 
         System.setProperty( "chromeoptions.args", "--no-sandbox --disable-dev-shm-usage" );
 
