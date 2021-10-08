0b210f3ef4df458918881664e9e585af3052a4d6
art1988 <art.khassanov@gmail.com>
2021-10-08 17:41:29 +0500

    timings for at-167

--- ParleyPro Autotests/src/test/java/tests/classic/at167/CreateContractAndUploadFirstDoc.java
+++ ParleyPro Autotests/src/test/java/tests/classic/at167/CreateContractAndUploadFirstDoc.java
@@ -71,8 +71,8 @@ public class CreateContractAndUploadFirstDoc
         $(".notification-stack").waitUntil(Condition.appear, 20_000).shouldHave(Condition.text("is now in negotiation"));
 
         // wait until color of discussion's icons become purple
-        $(".contract-header__status .discussion-indicator").waitUntil(Condition.cssValue("color", "rgba(127, 111, 207, 1)"), 20_000);
-        $$(".document__body .discussion-indicator").first().waitUntil(Condition.cssValue("color", "rgba(127, 111, 207, 1)"), 20_000);
+        $(".contract-header__status .discussion-indicator").waitUntil(Condition.cssValue("color", "rgba(127, 111, 207, 1)"), 60_000);
+        $$(".document__body .discussion-indicator").first().waitUntil(Condition.cssValue("color", "rgba(127, 111, 207, 1)"), 60_000);
 
         logger.info("Making sure that all discussions become external ...");
         Assert.assertEquals(openedContract.getAmountOfContractDiscussion(), "4", "Amount of opened discussions is wrong !!!");
