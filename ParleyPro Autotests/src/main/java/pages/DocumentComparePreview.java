999d80fbf5f0f56e13d30372a8594426ca37b493
art1988 <art.khassanov@gmail.com>
2021-10-12 16:05:06 +0500

    improvement for DocumentComparePreview

--- ParleyPro Autotests/src/main/java/pages/DocumentComparePreview.java
+++ ParleyPro Autotests/src/main/java/pages/DocumentComparePreview.java
@@ -25,6 +25,8 @@ public class DocumentComparePreview
      */
     public DocumentComparePreview(String documentName, String contractName)
     {
+        try { Thread.sleep(2_000); } catch (InterruptedException e) { e.printStackTrace(); }
+
         logger.info("Waiting until spinner will disappear [up to 2 minutes]...");
         $(".spinner").waitUntil(Condition.disappear, 60_000 * 2);
 
