f28739691c65897abaf3513fa3d8fff849bab8de
art1988 <art.khassanov@gmail.com>
2021-10-15 18:13:35 +0500

    finished with AT-182; test was added to Regression suite

---
+++ ParleyPro Autotests/src/test/java/tests/regression/at182/AddPostsAndCheck.java
@@ -0,0 +1,54 @@
+package tests.regression.at182;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.OpenedContract;
+import pages.OpenedDiscussion;
+import pages.subelements.CKEditorActive;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class AddPostsAndCheck
+{
+    private OpenedContract openedContract;
+    private static Logger logger = Logger.getLogger(AddPostsAndCheck.class);
+
+    @Test
+    @Description("This test creates new paragraph with text change, clicks by the last post, adds comment to the same paragraph and checks" +
+            " that comment was added and no grey screen happened. ")
+    public void addPostsAndCheck() throws InterruptedException
+    {
+        openedContract = new OpenedContract();
+
+        logger.info("Adding ' - test' at the end of paragraph...");
+        openedContract.clickByParagraph("June 23")
+                      .sendSpecificKeys(new CharSequence[]{Keys.END, " - test"})
+                      .clickPost();
+
+        $(".notification-stack").shouldBe(Condition.visible).shouldHave(Condition.text(" has been successfully created."));
+
+        logger.info("Clicking by just created discussion icon...");
+        OpenedDiscussion openedDiscussion = openedContract.clickByDiscussionIconSoft("June 23");
+        Assert.assertEquals(openedDiscussion.getCountOfPosts(), "1", "Count of posts is wrong !!!");
+
+        logger.info("Clicking by last post...");
+        $(".discussion2__body .discussion2-post .text-reset").click();
+        String commentToAdd = "COMMENT_ABC";
+        new CKEditorActive().setComment(commentToAdd).clickPost();
+
+        $(".notification-stack").shouldBe(Condition.visible).shouldHave(Condition.text(" has been successfully created."));
+
+        logger.info("Checking that comment was added to post...");
+        $(".discussion2__body .discussion2-post .discussion2-post__comment div").shouldHave(Condition.exactText(commentToAdd));
+
+        Screenshoter.makeScreenshot();
+    }
+}
---
+++ ParleyPro Autotests/src/test/java/tests/regression/at182/CreateContractAndUploadDoc.java
@@ -0,0 +1,49 @@
+package tests.regression.at182;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import constants.Const;
+import forms.ContractInformation;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.AddDocuments;
+import pages.DashboardPage;
+import pages.OpenedContract;
+import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@Listeners({ScreenShotOnFailListener.class})
+public class CreateContractAndUploadDoc
+{
+    @Test
+    @Description("This test uploads doc for newly created contract and switches it to Review state.")
+    public void createContractAndUploadDoc()
+    {
+        ContractInformation contractInformation = new DashboardPage().getSideBar()
+                                                                     .clickInProgressContracts(true)
+                                                                     .clickNewContractButton();
+
+        contractInformation.setContractTitle("AT-182 CTR");
+        contractInformation.setContractingRegion("region1");
+        contractInformation.setContractingCountry("country1");
+        contractInformation.setContractEntity("entity1");
+        contractInformation.setContractingDepartment("department1");
+        contractInformation.setContractCategory("category1");
+        contractInformation.setContractType("type1");
+        contractInformation.clickSave();
+
+        new AddDocuments().clickUploadMyTeamDocuments( Const.DOC_AT166_ONE );
+
+        $(".notification-stack").should(Condition.appear);
+        $(".notification-stack").waitUntil(Condition.disappear, 25_000);
+
+        new OpenedContract().switchDocumentToReview("AT-166_Manufacturing Agreement_1").clickStart();
+        $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(2)).shouldHave(CollectionCondition.exactTexts("REVIEW\n(1)", "REVIEW"));
+
+        Screenshoter.makeScreenshot();
+    }
+}
