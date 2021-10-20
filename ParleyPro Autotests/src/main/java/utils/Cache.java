28ccdf6a64dd36f9d358be2e6d478f09a64c893e
art1988 <art.khassanov@gmail.com>
2021-10-20 17:55:36 +0500

    Cache improvements

--- ParleyPro Autotests/src/main/java/utils/Cache.java
+++ ParleyPro Autotests/src/main/java/utils/Cache.java
@@ -1,26 +1,52 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Util class that saves Contract title within one single test.
- * May be useful to retrieve 'Contract title' from another test-class within one single test.
+ * Util class that saves useful data within one single test.
+ * May be useful to retrieve 'Contract title', 'Current tab handle' from another test-class within one single test.
  */
 public class Cache
 {
-    public static Cache CACHE_INSTANCE = new Cache(); // singleton instance
+    private static Cache CACHE_INSTANCE = new Cache(); // singleton instance
 
-    private static String cachedContractTitle;
+    private static Map<String, String> hashTable = new HashMap<>();
+
+    private static String KEY_CONTRACT_TITLE     = "KEY_CONTRACT_TITLE";
+    private static String KEY_CURRENT_TAB_HANDLE = "KEY_CURRENT_TAB_HANDLE";
+    private static String KEY_TAB_1_HANDLE       = "KEY_TAB_1_HANDLE";
 
     private Cache() {}
 
-
     public void setContractTitle(String contractTitle)
     {
-        cachedContractTitle = contractTitle;
+        hashTable.put(KEY_CONTRACT_TITLE, contractTitle);
     }
 
     public String getCachedContractTitle()
     {
-        return cachedContractTitle;
+        return hashTable.get(KEY_CONTRACT_TITLE);
+    }
+
+    public void setCurrentTabHandle(String currentTabHandle)
+    {
+        hashTable.put(KEY_CURRENT_TAB_HANDLE, currentTabHandle);
+    }
+
+    public String getCachedCurrentTabHandle()
+    {
+        return hashTable.get(KEY_CURRENT_TAB_HANDLE);
+    }
+
+    public void setTab1Handle(String tab1Handle)
+    {
+        hashTable.put(KEY_TAB_1_HANDLE, tab1Handle);
+    }
+
+    public String getCachedTab1Handle()
+    {
+        return hashTable.get(KEY_TAB_1_HANDLE);
     }
 
     public static Cache getInstance()
