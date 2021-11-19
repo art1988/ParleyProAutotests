097fd133cbbf6e0ec2d0196144bd11aa3b9cc019
art1988 <art.khassanov@gmail.com>
2021-11-19 19:25:55 +0500

    EditDocumentPage: were added clickByTextColor, getEditorBodyInstance

--- ParleyPro Autotests/src/main/java/pages/EditDocumentPage.java
+++ ParleyPro Autotests/src/main/java/pages/EditDocumentPage.java
@@ -4,10 +4,12 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import forms.editor_toolbar.TableProperties;
+import forms.editor_toolbar.TextColorPicker;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 /**
  * This class represents opened CKEditor (WYSIWYG editor with buttons).
@@ -24,38 +26,59 @@ public class EditDocumentPage
     {
         if( shouldHaveFormattingLabel )
         {
-            documentName.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText(documentTitle + "\n" + "FORMATTING"));
+            documentName.shouldBe(Condition.visible).shouldHave(Condition.exactText(documentTitle + "\n" + "FORMATTING"));
         }
         else
         {
-            documentName.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText(documentTitle));
+            documentName.shouldBe(Condition.visible).shouldHave(Condition.exactText(documentTitle));
         }
 
-        $(".editor-popup__body").waitUntil(Condition.visible, 7_000);
-        $("#editor-toolbar").waitUntil(Condition.visible, 7_000);
+        $(".editor-popup__body").shouldBe(Condition.visible);
+        $("#editor-toolbar").shouldBe(Condition.visible);
+    }
+
+    public TextColorPicker clickByTextColor()
+    {
+        $("#editor-toolbar a[title='Text Color']").shouldBe(Condition.visible).click();
+
+        logger.info("Text Color button was clicked");
+
+        return new TextColorPicker();
     }
 
     public TableProperties insertTable()
     {
-        $("#editor-toolbar a[title='Table']").waitUntil(Condition.visible, 7__000).click();
+        $("#editor-toolbar a[title='Table']").shouldBe(Condition.visible).click();
 
-        logger.info("Insert Table");
+        logger.info("Insert Table button was clicked");
 
         return new TableProperties();
     }
 
+    public WebElement getEditorBodyInstance()
+    {
+        StringBuffer jsCode = new StringBuffer("var names = [];");
+        jsCode.append("for (var i in CKEDITOR.instances) { names.push(CKEDITOR.instances[i]) }");
+        jsCode.append("var editor_instance = names[0];");
+        jsCode.append("return $(editor_instance.element.$)[0];");
+
+        WebElement editorBody = Selenide.executeJavaScript(jsCode.toString());
+
+        return editorBody;
+    }
+
     public void clickSave()
     {
-        WebElement saveButton = Selenide.executeJavaScript("return $('.modal-content button span:contains(\"Save\")')[0]");
-        $(saveButton).click();
+        $$(".modal-content button").filterBy(Condition.text("Save")).first().click();
 
         logger.info("Save button was clicked");
+
+        $(".modal-content").should(Condition.disappear);
     }
 
     public void clickCancel()
     {
-        WebElement cancelButton = Selenide.executeJavaScript("return $('.modal-content button span:contains(\"Cancel\")')[0]");
-        $(cancelButton).click();
+        $$(".modal-content button").filterBy(Condition.text("Cancel")).first().click();
 
         logger.info("Cancel button was clicked");
     }
