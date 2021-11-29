e926bd4370b4293f7c634c66b02e9026e7710522
art1988 <art.khassanov@gmail.com>
2021-11-29 19:08:19 +0500

    improvements for StartExternalDiscussion; were added clickNext, clickSend

--- ParleyPro Autotests/src/main/java/forms/StartExternalDiscussion.java
+++ ParleyPro Autotests/src/main/java/forms/StartExternalDiscussion.java
@@ -1,11 +1,11 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 
-import static com.codeborne.selenide.Condition.or;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 /**
  * Represents popup that appears after clicking POST of External discussion.
@@ -13,28 +13,62 @@ import static com.codeborne.selenide.Selenide.$;
  */
 public class StartExternalDiscussion
 {
-    private SelenideElement postExternallyButton = $(".button.btn-common.btn.btn-primary");
-
-
     private static Logger logger = Logger.getLogger(StartExternalDiscussion.class);
 
+    /**
+     * Use this constructor if Counterparty is known
+     * @param text partial text of the discussion
+     * @param cpOrganization
+     */
     public StartExternalDiscussion(String text, String cpOrganization)
     {
-        $(".modal-body-title").waitUntil(Condition.visible, 7_000)
+        $(".modal-body-title").shouldBe(visible)
                 .shouldHave(or("message on popup", Condition.text("You are about to start an external discussion \"" + text + "\"."),
                                                          Condition.text("You are about to make an external post in")));
 
-        $(".modal-body-description").waitUntil(Condition.visible, 7_000)
+        $(".modal-body-description").shouldBe(visible)
                 .shouldHave(or("message on popup", Condition.text("Once started, it will be visible to " + cpOrganization),
                                                          Condition.text("Once posted, it will be visible to " + cpOrganization)));
     }
 
+    /**
+     * Use this constructor if no Counterparty selected
+     * @param text partial text of the discussion
+     */
+    public StartExternalDiscussion(String text)
+    {
+        $(".modal-body-title").shouldBe(visible)
+                .shouldHave(or("message on popup", Condition.text("You are about to start an external discussion \"" + text + "\""),
+                                                         Condition.text("You are about to make an external post in")));
+
+        $(".modal-content .share-documents__message").shouldHave(exactText("Once started, this discussion and selected documents will be visible to the Counterparty"));
+    }
+
     public void clickPostExternally()
     {
-        postExternallyButton.click();
+        $$(".modal-footer button").filterBy(text("POST EXTERNALLY")).first().shouldBe(visible, enabled).click();
 
         logger.info("POST EXTERNALLY button was clicked...");
 
         $(".modal-content").should(Condition.disappear);
+    }
+
+    public StartExternalDiscussion clickNext()
+    {
+        $$(".modal-footer button").filterBy(text("NEXT")).first().shouldBe(visible, enabled).click();
+
+        logger.info("NEXT button was clicked...");
+
+        return this;
+    }
+
+    public StartExternalDiscussion clickSend(String text, String cpOrganization)
+    {
+        // wait until spinner for CCN field will disappear
+        $(".select__loading").should(disappear);
+
+        $$(".modal-footer button").filterBy(text("SEND")).first().shouldBe(visible, enabled).click();
+
+        return new StartExternalDiscussion(text, cpOrganization);
     }
 }
