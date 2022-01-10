1b8084640f0d03a18fa12b54fab21a3e99bd47a5
art1988 <art.khassanov@gmail.com>
2022-01-10 14:17:45 +0500

    finished with at-210

--- ParleyPro Autotests/src/test/java/tests/requests/at210/ConvertRequestToContract.java
+++ ParleyPro Autotests/src/test/java/tests/requests/at210/ConvertRequestToContract.java
@@ -1,13 +1,18 @@
 package tests.requests.at210;
 
+import com.codeborne.selenide.Condition;
 import constants.Const;
 import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.ContractInfo;
+import pages.DashboardPage;
 import pages.LoginPage;
 import utils.ScreenShotOnFailListener;
+import utils.Screenshoter;
+
+import static com.codeborne.selenide.Selenide.$;
 
 @Listeners({ScreenShotOnFailListener.class})
 public class ConvertRequestToContract
@@ -15,9 +20,9 @@ public class ConvertRequestToContract
     private static Logger logger = Logger.getLogger(ConvertRequestToContract.class);
 
 
-    @Test
+    @Test(priority = 1)
     @Description("Final check happens here. This test converts request to contract and checks that ‘Request' label disappears and 'Add Documents’ section is shown.")
-    public void convertRequestToContract() throws InterruptedException
+    public void convertRequestToContract()
     {
         logger.info("Login as user with full rights (Felix)...");
 
@@ -37,6 +42,17 @@ public class ConvertRequestToContract
         contractInfo.setContractType("type1");
         contractInfo.clickSave();
 
-        Thread.sleep(5_000);
+        $(".label.label_theme_purple").should(Condition.disappear);
+        $(".documents-add__title").shouldBe(Condition.visible).shouldHave(Condition.exactText("Add Documents"));
+        $(".js-upload-my-team-document-btn").shouldBe(Condition.visible);
+        $(".js-upload-cp-document-btn").shouldBe(Condition.visible);
+
+        Screenshoter.makeScreenshot();
+    }
+
+    @Test(priority = 2)
+    public void logoutAsFelix()
+    {
+        new DashboardPage().getSideBar().logout();
     }
 }
