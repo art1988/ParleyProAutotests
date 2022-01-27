eb4a2820ed47e506ae399c94d091c505bc4318af
art1988 <art.khassanov@gmail.com>
2022-01-27 15:32:08 +0500

    was added check of email after termination

--- ParleyPro Autotests/src/test/java/tests/basics/at61/TerminateExecuted.java
+++ ParleyPro Autotests/src/test/java/tests/basics/at61/TerminateExecuted.java
@@ -6,6 +6,7 @@ import constants.Const;
 import forms.ContractInformation;
 import io.qameta.allure.Description;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.AddDocuments;
@@ -13,6 +14,7 @@ import pages.ContractInfo;
 import pages.DashboardPage;
 import pages.OpenedContract;
 import pages.subelements.SideBar;
+import utils.EmailChecker;
 import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
@@ -22,12 +24,16 @@ import static com.codeborne.selenide.Selenide.$$;
 @Listeners({ScreenShotOnFailListener.class})
 public class TerminateExecuted
 {
+    private String host = "pop.gmail.com";
+    private String username = "arthur.khasanov@parleypro.com";
+    private String password = "ParGd881";
+
     private static Logger logger = Logger.getLogger(TerminateExecuted.class);
 
 
     @Test
     @Description("This test terminates executed contract and check it's status.")
-    public void terminateExecuted()
+    public void terminateExecuted() throws InterruptedException
     {
         SideBar sideBar = new DashboardPage().getSideBar();
 
@@ -48,6 +54,9 @@ public class TerminateExecuted
         ContractInfo contractInfo = new ContractInfo();
         contractInfo.setSignatureDate();
         contractInfo.setEffectiveDate();
+        contractInfo.setExpirationEmailTo(Const.PREDEFINED_INTERNAL_USER_1.getEmail());
+        contractInfo.setNotification("1 day");
+        contractInfo.setExpirationDate("Jan 17, 2024");
         contractInfo.clickSave();
 
         $$(".lifecycle__item.active").shouldHave(CollectionCondition.size(1)).first().shouldHave(Condition.exactText("MANAGED"));
@@ -64,5 +73,11 @@ public class TerminateExecuted
         $(".contracts-list__cell-stage").shouldBe(Condition.visible).shouldHave(Condition.exactText("TERMINATED"));
 
         Screenshoter.makeScreenshot();
+
+        logger.info("Waiting for 60 seconds to make sure that email has been delivered...");
+        Thread.sleep(60_000);
+
+        Assert.assertTrue(EmailChecker.assertEmailBySubject(host, username, password, "AT-61: terminate me contract has been terminated"),
+                "Email with subject: 'AT-61: terminate me contract has been terminated' was not found !!!");
     }
 }
