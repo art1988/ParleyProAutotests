554f0294ac8f42dcd7b4ae701c433cd91f102351
art1988 <art.khassanov@gmail.com>
2022-02-07 17:55:36 +0500

    updated selector

--- ParleyPro Autotests/src/main/java/forms/ReassignChiefNegotiator.java
+++ ParleyPro Autotests/src/main/java/forms/ReassignChiefNegotiator.java
@@ -26,8 +26,9 @@ public class ReassignChiefNegotiator
         SelenideElement input = $(".modal-content input");
 
         input.sendKeys(cnEmail);
-        $(".Select-loading").waitUntil(Condition.disappear, 25_000);
+        $(".new_select__loading-indicator").waitUntil(Condition.disappear, 25_000);
 
+        Thread.sleep(1_000);
         input.sendKeys(Keys.ARROW_DOWN);
         Thread.sleep(500);
         input.pressEnter();
