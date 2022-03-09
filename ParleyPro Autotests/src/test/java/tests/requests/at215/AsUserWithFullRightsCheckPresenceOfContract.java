c775e4bc1a093d42803d7870130d6c4a9d19662a
art1988 <art.khassanov@gmail.com>
2022-03-09 19:18:58 +0500

    improvements for at-215

--- ParleyPro Autotests/src/test/java/tests/requests/at215/AsUserWithFullRightsCheckPresenceOfContract.java
+++ ParleyPro Autotests/src/test/java/tests/requests/at215/AsUserWithFullRightsCheckPresenceOfContract.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.OpenedContract;
 import pages.subelements.SideBar;
 import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
@@ -38,11 +39,12 @@ public class AsUserWithFullRightsCheckPresenceOfContract
         $(byText("AT-215 Request")).shouldBe(Condition.visible);
 
         sideBar.clickInProgressContracts(false).selectContract("AT-215 Request");
+        new OpenedContract();
 
         logger.info("Hover over user icon FW and check role...");
         $$(".contract-header-users .user").filterBy(Condition.exactText("FW")).first().hover();
-        $(".rc-tooltip-content").waitUntil(Condition.appear, 30_000);
-        $(".rc-tooltip-inner .spinner").waitUntil(Condition.disappear, 30_000);
+        $(".rc-tooltip-content").should(Condition.appear);
+        $(".rc-tooltip-inner .spinner").should(Condition.disappear);
         $(".rc-tooltip-inner .contract-user__status").shouldHave(Condition.exactText("Requester"));
 
         Screenshoter.makeScreenshot();
