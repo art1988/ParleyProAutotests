e429d13a023101cf1e1e4a823b34e6369bbd3ce8
art1988 <art.khassanov@gmail.com>
2022-03-11 15:41:58 +0500

    CancelContract: were added checkLeaveCommentCheckbox, setMessage

--- ParleyPro Autotests/src/main/java/forms/CancelContract.java
+++ ParleyPro Autotests/src/main/java/forms/CancelContract.java
@@ -2,7 +2,9 @@ package forms;
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.interactions.Actions;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -17,6 +19,21 @@ public class CancelContract
         $(".modal-body-title").shouldBe(Condition.visible).shouldHave(Condition.text("You have asked to cancel contract"));
 
         $$(".modal-content button").shouldHave(CollectionCondition.size(2));
+    }
+
+    public CancelContract checkLeaveCommentCheckbox()
+    {
+        Actions actionProvider = new Actions(WebDriverRunner.getWebDriver());
+        actionProvider.click($("input[type='checkbox']").toWebElement()).build().perform();
+
+        return this;
+    }
+
+    public CancelContract setMessage(String commentMessage)
+    {
+        $("textarea").shouldBe(Condition.visible, Condition.enabled).sendKeys(commentMessage);
+
+        return this;
     }
 
     public void clickCancelContract()
