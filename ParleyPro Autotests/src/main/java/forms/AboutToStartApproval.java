6d5b27a34f22a32bfd24a03d753cac888a83aedf
art1988 <art.khassanov@gmail.com>
2022-03-17 12:21:33 +0500

    AboutToStartApproval: were added log messages

--- ParleyPro Autotests/src/main/java/forms/AboutToStartApproval.java
+++ ParleyPro Autotests/src/main/java/forms/AboutToStartApproval.java
@@ -1,13 +1,19 @@
 package forms;
 
 import com.codeborne.selenide.Condition;
+import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
+/**
+ * Form that appears after clicking by APPROVAL lifecycle icon
+ */
 public class AboutToStartApproval
 {
     private String contractName;
+
+    private static Logger logger = Logger.getLogger(AboutToStartApproval.class);
 
 
     public AboutToStartApproval(String contractName)
@@ -21,11 +27,15 @@ public class AboutToStartApproval
     {
         $$(".modal-footer button").filterBy(Condition.exactText("NEXT")).first().shouldBe(Condition.visible, Condition.enabled).click();
 
+        logger.info("NEXT button was clicked");
+
         return new ConfirmApprovers(contractName);
     }
 
     public void clickCancel()
     {
         $$(".modal-footer button").filterBy(Condition.exactText("CANCEL")).first().shouldBe(Condition.visible, Condition.enabled).click();
+
+        logger.info("CANCEL button was clicked");
     }
 }
