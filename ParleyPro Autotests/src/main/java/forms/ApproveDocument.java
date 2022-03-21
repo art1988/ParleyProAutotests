edd2174962cdc3942ae5f773aa131b6ecd8ea868
art1988 <art.khassanov@gmail.com>
2022-03-21 12:52:32 +0500

    ApproveDocument: improvements

--- ParleyPro Autotests/src/main/java/forms/ApproveDocument.java
+++ ParleyPro Autotests/src/main/java/forms/ApproveDocument.java
@@ -6,6 +6,9 @@ import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
 
+/**
+ * Form that appears after clicking 'Approve' button in document header
+ */
 public class ApproveDocument
 {
     private SelenideElement title         = $(".modal-body-title");
@@ -16,12 +19,12 @@ public class ApproveDocument
 
     public ApproveDocument(String documentName)
     {
-        title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Are you sure you want to approve document \"" + documentName + "\"?"));
+        title.shouldBe(Condition.visible).shouldHave(Condition.exactText("Are you sure you want to approve document \"" + documentName + "\"?"));
     }
 
     public void clickApproveButton()
     {
-        approveButton.click();
+        approveButton.shouldBe(Condition.visible, Condition.enabled).click();
 
         logger.info("APPROVE button was clicked");
     }
