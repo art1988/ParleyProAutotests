789345243f83caee449ac5aad3fccc23084e335e
art1988 <art.khassanov@gmail.com>
2022-03-22 19:08:15 +0500

    AboutToStartApproval: was added checkDocument

--- ParleyPro Autotests/src/main/java/forms/AboutToStartApproval.java
+++ ParleyPro Autotests/src/main/java/forms/AboutToStartApproval.java
@@ -23,6 +23,19 @@ public class AboutToStartApproval
         $(".modal-body-title").shouldBe(Condition.visible).shouldHave(Condition.exactText("You are about to start approval for contract \"" + contractName + "\""));
     }
 
+    /**
+     * Click by checkbox for the given documentName. Use this method to check or uncheck document.
+     * @param documentName
+     */
+    public AboutToStartApproval checkDocument(String documentName)
+    {
+        $$(".checkbox.with_label").filterBy(Condition.exactText(documentName)).first().click();
+
+        logger.info("The following document was checked/unchecked: " + documentName);
+
+        return this;
+    }
+
     public ConfirmApprovers clickNext()
     {
         $$(".modal-footer button").filterBy(Condition.exactText("NEXT")).first().shouldBe(Condition.visible, Condition.enabled).click();
