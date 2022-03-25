51888e12ccb1c08230a8ba72cd7c6aec1b4bf98b
art1988 <art.khassanov@gmail.com>
2022-03-25 18:34:54 +0500

    AuditTrail: was added collectAllEvents

--- ParleyPro Autotests/src/main/java/pages/AuditTrail.java
+++ ParleyPro Autotests/src/main/java/pages/AuditTrail.java
@@ -2,9 +2,14 @@ package pages;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import model.AuditTrailEvent;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class AuditTrail
 {
@@ -19,6 +24,15 @@ public class AuditTrail
         title.waitUntil(Condition.visible, 7_000).shouldHave(Condition.exactText("Audit trail"));
 
         $(".timeline").waitUntil(Condition.visible, 7_000);
+    }
+
+    public List<AuditTrailEvent> collectAllEvents()
+    {
+        List<AuditTrailEvent> allEvents = new ArrayList<>();
+
+        $$(".timeline-heading h4").stream().forEach(eventTitle -> allEvents.add(new AuditTrailEvent(eventTitle.getText())));
+
+        return allEvents;
     }
 
     public void clickOk()
