39d79d937f70ad39f21987180d2d62ac112765e4
art1988 <art.khassanov@gmail.com>
2022-03-31 19:35:09 +0500

    AT-245: in-progress

--- ParleyPro Autotests/src/test/java/tests/approval_workflow/at245/CheckButtonsForTeamInApproval.java
+++ ParleyPro Autotests/src/test/java/tests/approval_workflow/at245/CheckButtonsForTeamInApproval.java
@@ -3,18 +3,23 @@ package tests.approval_workflow.at245;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
 import constants.Const;
 import forms.ContractInformation;
+import forms.ResendInviteForm;
 import forms.workflows.ApprovalWorkflow;
 import io.qameta.allure.Step;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.AddDocuments;
 import pages.DashboardPage;
+import pages.LoginPage;
 import pages.OpenedContract;
 import pages.subelements.SideBar;
+import utils.EmailChecker;
 import utils.ScreenShotOnFailListener;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -23,6 +28,8 @@ import static com.codeborne.selenide.Selenide.$$;
 @Listeners({ScreenShotOnFailListener.class})
 public class CheckButtonsForTeamInApproval
 {
+    private static final String CONTRACT_NAME = "AT-245 - ResendInvite_and_SendMessage";
+
     private SideBar sideBar;
     private OpenedContract openedContract;
 
@@ -44,7 +51,7 @@ public class CheckButtonsForTeamInApproval
         logger.info("Creating contract...");
         ContractInformation contractInformationForm = sideBar.clickInProgressContracts(true).clickNewContractButton();
 
-        contractInformationForm.setContractTitle("AT-245 - ResendInvite_and_SendMessage");
+        contractInformationForm.setContractTitle(CONTRACT_NAME);
         contractInformationForm.setContractCurrency("GBP");
         contractInformationForm.setContractValue("1000");
         contractInformationForm.setContractingRegion("region1");
@@ -70,25 +77,69 @@ public class CheckButtonsForTeamInApproval
     public void checkButtonsForTeamInApproval() throws InterruptedException
     {
         checkSendReminderPresenceAndClick();
+        checkContractApprovalRequestEmail();
+        loginAsTeamMemberAndVisitContract();
     }
 
-    @Step
+    @Step("Hover team icons in document header to find team by the name 'Team 2' and check that it has SEND A REMINDER button.")
     public void checkSendReminderPresenceAndClick() throws InterruptedException
     {
         logger.info("Hover Team 2...");
 
-        $(".header-users").shouldBe(Condition.visible).hover();
-
-        ElementsCollection userIcons = $$(".header-users span").shouldHave(CollectionCondition.size(5));
-
-        for( int i = 0; i < userIcons.size(); i++ )
+        ResendInviteForm resendInviteForm;
+        ElementsCollection teamIcons = $$(".header-users > .team-icon.team").shouldHave(CollectionCondition.size(2));
+        for( int i = 0; i < teamIcons.size(); i++ )
         {
-            userIcons.get(i).shouldBe(Condition.visible, Condition.enabled).hover();
+            StringBuffer jsCode = new StringBuffer("var event = new MouseEvent('mouseover', {bubbles: true, cancelable: true}); ");
+            jsCode.append("$('.header-users > .team-icon.team').eq(" + i + ")[0].dispatchEvent(event);");
+            Selenide.executeJavaScript(jsCode.toString());
 
+            Thread.sleep(2_000);
             if( $(".users-tooltip__team-name").getText().contains("Team #2") )
             {
                 $(".rc-tooltip-content button").shouldBe(Condition.visible, Condition.enabled).shouldHave(Condition.exactText("SEND A REMINDER")).click();
+                resendInviteForm = new ResendInviteForm(null); // there is no parentForm => pass null
+                resendInviteForm.clickResend();
+                break;
+            }
+            else
+            {
+                Thread.sleep(1_000);
+                Selenide.executeJavaScript("$('.rc-tooltip-inner').hide()");
+                Thread.sleep(1_000);
+                continue;
             }
         }
+
+        $(".notification-stack").shouldBe(Condition.visible).shouldHave(Condition.text("Reminder email has been successfully sent"));
+    }
+
+    @Step("Check that email with subject: 'Contract approval request' is present.")
+    public void checkContractApprovalRequestEmail() throws InterruptedException
+    {
+        logger.info("Waiting for 60 seconds to make sure that email has been delivered...");
+        Thread.sleep(60_000);
+        Assert.assertTrue(EmailChecker.assertEmailBySubject(Const.HOST_GMAIL, Const.USERNAME_GMAIL, Const.PASSWORD_GMAIL, "Contract \"" + CONTRACT_NAME + "\" approval request"),
+                "Email with subject: " + "Contract \"" + CONTRACT_NAME + "\" approval request" + " was not found !!!");
+    }
+
+    @Step("Login as User Felix and visit contract.")
+    public void loginAsTeamMemberAndVisitContract()
+    {
+        LoginPage loginPage = sideBar.logout();
+
+        loginPage.setEmail(Const.USER_FELIX.getEmail());
+        loginPage.setPassword(Const.USER_FELIX.getPassword());
+
+        sideBar = loginPage.clickSignIn().getSideBar();
+
+        sideBar.clickInProgressContracts(false).selectContract(CONTRACT_NAME);
+        openedContract = new OpenedContract();
+    }
+
+    @Step("Check that for team Autotest_TEAM_3 [EDITED] button MESSAGE become visible.")
+    public void loginAsMyTeamCNAndCheckMessageButtonForTeam3()
+    {
+
     }
 }
