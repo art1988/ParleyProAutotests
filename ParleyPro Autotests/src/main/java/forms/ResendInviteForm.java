cc5e4aa74c83ad05f251de898a30c9c0d85596a4
art1988 <art.khassanov@gmail.com>
2022-03-31 19:34:55 +0500

    ResendInviteForm: some improvements; was added clickResend

--- ParleyPro Autotests/src/main/java/forms/ResendInviteForm.java
+++ ParleyPro Autotests/src/main/java/forms/ResendInviteForm.java
@@ -5,14 +5,15 @@ import com.codeborne.selenide.SelenideElement;
 import org.apache.log4j.Logger;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 /**
- * Represents form that appears after clicking on 'Resend invite' svg icon in Share form
+ * Represents form that appears after clicking on 'Resend invite' svg icon on Share form or via team's tooltip
  */
 public class ResendInviteForm
 {
     private ShareForm parentForm;
-    private SelenideElement messageTextArea = $(".item.active .textarea.input__input");
+    private SelenideElement messageTextArea = $(".item.active .textarea.input__input, .modal-body-title");
     private SelenideElement sendButton      = $(".item.active ._button.scheme_blue.size_lg.manage-users-new__submit");
 
 
@@ -22,17 +23,25 @@ public class ResendInviteForm
     {
         this.parentForm = parentForm;
 
-        $(".modal-confirm-message__title").waitUntil(Condition.visible, 5_000).shouldHave(Condition.exactText("Resend invite"));
-
-        sendButton.waitUntil(Condition.visible, 5_000);
+        $(".modal-confirm-message__title").shouldBe(Condition.visible).shouldHave(Condition.exactText("Resend invite"));
     }
 
     public ShareForm clickSend()
     {
-        sendButton.click();
+        sendButton.shouldBe(Condition.visible, Condition.enabled).click();
 
         logger.info("SEND button was clicked...");
 
         return parentForm;
+    }
+
+    /**
+     * RESEND button is available on team's tooltip
+     */
+    public void clickResend()
+    {
+        $$(".modal-footer button").filterBy(Condition.exactText("RESEND")).first().shouldBe(Condition.visible, Condition.enabled).click();
+
+        logger.info("RESEND button was clicked...");
     }
 }
