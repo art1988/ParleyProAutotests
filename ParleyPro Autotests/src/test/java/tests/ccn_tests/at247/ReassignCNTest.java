129360543220c0feb47fdc6b1eff03a843810f97
art1988 <art.khassanov@gmail.com>
2022-04-06 12:03:02 +0500

    finished with AT-247

--- ParleyPro Autotests/src/test/java/tests/ccn_tests/at247/ReassignCNTest.java
+++ ParleyPro Autotests/src/test/java/tests/ccn_tests/at247/ReassignCNTest.java
@@ -9,6 +9,7 @@ import forms.ContractInformation;
 import io.qameta.allure.Step;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -21,6 +22,7 @@ import utils.EmailChecker;
 import utils.ScreenShotOnFailListener;
 import utils.Screenshoter;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
@@ -66,6 +68,7 @@ public class ReassignCNTest
         asCCNReassignChiefNegotiator();
         openContractAndCheckUsersIconsInDocHeader();
         checkEmail();
+        loginAsNewChiefNegotiator();
     }
 
     @Step
@@ -115,5 +118,44 @@ public class ReassignCNTest
         Thread.sleep(60_000);
         Assert.assertTrue(EmailChecker.assertEmailBySubject(Const.HOST_GMAIL, Const.USERNAME_GMAIL, Const.PASSWORD_GMAIL, "[qa-autotests] CCN AT fn CCN AT ln shared contract " + "\"" + CONTRACT_NAME + "\"" + " with you"),
                 "Email with subject: '[qa-autotests] CCN AT fn CCN AT ln shared contract with you' was not found !!!");
+    }
+
+    @Step
+    public void loginAsNewChiefNegotiator()
+    {
+        LoginPage loginPage = sideBar.logout();
+
+        loginPage.setEmail(Const.PREDEFINED_CCN2.getEmail());
+        loginPage.setPassword(Const.PREDEFINED_CCN2.getPassword());
+        sideBar = loginPage.clickSignIn(new SideBarItems[]{SideBarItems.IN_PROGRESS_CONTRACTS, SideBarItems.EXECUTED_CONTRACTS}).getSideBar();
+
+        sideBar.clickInProgressContracts(false).selectContract(CONTRACT_NAME);
+        openedContract = new OpenedContract();
+
+        logger.info("Check presence of paragraphs...");
+        for(int pNum = 1; pNum <= 7; pNum++) $(withText("Paragraph " + pNum)).shouldBe(Condition.visible);
+
+        logger.info("Checking user icons in document header...");
+        $$(".header-users .user").shouldHave(CollectionCondition.size(3)).shouldHave(CollectionCondition.textsInAnyOrder("PF", "CC", "AL"));
+
+        $$(".header-users .user").findBy(Condition.exactText("PF")).hover();
+        $(".contract-user__section .contract-user__status").shouldBe(Condition.visible).shouldHave(Condition.exactText("Chief Negotiator"));
+        Screenshoter.makeScreenshot();
+
+        Selenide.executeJavaScript("$('.rc-tooltip-inner').hide(); $('.rc-tooltip-inner').remove()");
+        $$(".header-users .user").findBy(Condition.exactText("CC")).hover();
+        $(".contract-user__section .contract-user__status").shouldBe(Condition.visible).shouldHave(Condition.exactText("Lead"));
+        Screenshoter.makeScreenshot();
+    }
+
+    @AfterMethod
+    public void loginBackAsMyTeamCN()
+    {
+        LoginPage loginPage = sideBar.logout();
+
+        loginPage.setEmail(Const.PREDEFINED_USER_CN_ROLE.getEmail());
+        loginPage.setPassword(Const.PREDEFINED_USER_CN_ROLE.getPassword());
+
+        sideBar = loginPage.clickSignIn().getSideBar();
     }
 }
